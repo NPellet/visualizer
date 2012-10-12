301535f2592bd1b7140344311fa306e0d2d641be
Norman Pellet <norman.pellet@gmail.com>
2012-10-12 16:34:16 +0200

    Make checkbox and range bar work

--- scripts/modules/implementations/grid_selector/1.0/view.js
+++ scripts/modules/implementations/grid_selector/1.0/view.js
@@ -44,26 +44,33 @@ CI.Module.prototype._types.grid_selector.View.prototype = {
 				return;
 			var cols = moduleValue.categories;
 			var lines = moduleValue.variables;
+
+			this.cols = cols;
+			this.lines = lines;
+
 			this._selectors = {};
 			
 			html = '<table cellpadding="0" cellspacing="0">';
-			for(var i = -1, l = lines.length; i < l; i++) {
+			for(var i = -2, l = lines.length; i < l; i++) {
 				html += '<tr data-lineid="' + i + '" ' + (i > -1 ? ('style="background-color: ' + (lines[i].color) + '"') : '') + '>';
 
 				if(i == -1) // First line
-					html += '<th></th>';
+					html += '<td></td>';
+				else if(i == -2)
+					html += '<th>All</th>';
 				else
 					html += '<td>' + lines[i].label + '</td>';
-				
-				for(var j = 0, k = cols.length; j < k; j++) {
 
-					if(i == -1)
+				for(var j = 0, k = cols.length; j < k; j++) {
+					if(i == -2)
 						html += '<th width="' + (cols[j].selectorType == 'checkbox' ? '100' : '') + '" data-colid="' + j + '">' + cols[j].label + '</th>';
-					else {
+					else if(i == -1) {
+						html += '<td data-colid="' + j + '">' + this.getSelector(cols[j], null, j, i) + '</td>';
+					} else {
 						value = undefined;
 						if(this.module.getConfiguration()._data !== undefined && this.module.getConfiguration()._data[cols[j].name] !== undefined && this.module.getConfiguration()._data[cols[j].name][lines[i].name] !== undefined)
 							value = this.module.getConfiguration()._data[cols[j].name][lines[i].name];
-						html += '<td data-colid="' + j + '">' + this.getSelector(cols[j], lines[i], j, i, value) + '</td>';
+						html += '<td data-colid="' + j + '" ' + (i > -1 ? ('data-lineid="' + i + '"') : '') + '>' + this.getSelector(cols[j], lines[i], j, i, value) + '</td>';
 					}
 				}
 				html += '</tr>';
@@ -78,10 +85,14 @@ CI.Module.prototype._types.grid_selector.View.prototype = {
 
 	setEvents: function() {
 		var self = this;
-		$(this.dom).find('input[type="checkbox"]').bind('click', function() {
+		$(this.dom).find('input[type="checkbox"]').bind('click', function(event, data) {
 			var $this = $(this);
+			var checked = (data !== undefined) ? data : $(this).is(':checked');
 			var colId = $this.data('colid'), lineId = $this.data('lineid');
-			self.module.controller.selectorChanged(colId, lineId, $(this).is(':checked'));
+			if(lineId == undefined)
+				self.changeSelectors(colId, checked);
+			else
+				self.module.controller.selectorChanged(self.cols[colId].name, self.lines[lineId].name, checked);
 		});
 
 		$(this.dom).find('.ci-rangebar').each(function() {
@@ -95,14 +106,39 @@ CI.Module.prototype._types.grid_selector.View.prototype = {
 				step: 0.01,
 				values: [min, max],
 				slide: function(event, ui) {
+
 					var $this = $(this);
 					var colId = $this.data('colid'), lineId = $this.data('lineid');
-					self.module.controller.selectorChanged(colId, lineId, ui.values);
-					$this.prev().html(Math.round(ui.values[0] * 100) + " %");
-					$this.next().html(Math.round(ui.values[1] * 100) + " %");
+
+					if(lineId == undefined) {
+						self.changeSelectors(colId, ui.values);
+					}
+						
+					self.sliderUpdateValue($this, ui.values, colId, lineId);
 				}
 			});
 		});
+	},
+
+	sliderUpdateValue: function(el, value, colId, lineId) {
+		var self = this;
+		el.prev().html(Math.round(value[0] * 100) + " %");
+		el.next().html(Math.round(value[1] * 100) + " %");
+
+		if(lineId !== undefined) {
+			self.module.controller.selectorChanged(self.cols[colId].name, self.lines[lineId].name, value);
+		}
+	},
+
+	changeSelectors: function(colId, values) {
+		var dom = this.dom.children('table').find('td[data-colid="' + colId + '"][data-lineid]');
+		if(values instanceof Array) {
+			var el = dom.find('.ci-rangebar').slider('values', values);
+			this.sliderUpdateValue(el, values, colId, dom.data('lineid'))
+		} else if(values)
+			dom.find('input[type="checkbox"]').not(':checked').trigger('click', true);
+		else
+			dom.find('input[type="checkbox"]:checked').trigger('click', false);
 	},
 
 	getSelector: function(col, line, colId, lineId, value) {
@@ -110,14 +146,18 @@ CI.Module.prototype._types.grid_selector.View.prototype = {
 		if(col.selectorType == 'checkbox') {
 			var id = CI.Util.getNextUniqueId();
 			var defaultVal = value !== undefined ? value : col.defaultValue;
-			this._selectors[col.name][line.name] = defaultVal;
-			return '<input type="checkbox" id="' + id + '" ' + (defaultVal ? 'checked="checked"' : '') + '" data-colid="' + col.name + '" data-lineid="' + line.name + '" /><label for="' + id + '">&nbsp;</label>';
+
+			if(line)
+				this._selectors[col.name][line.name] = defaultVal;
+			return '<input type="checkbox" id="' + id + '" ' + (defaultVal ? 'checked="checked"' : '') + '" data-colid="' + colId + '" ' + (line ? ('data-lineid="' + lineId + '"') : '') + ' /><label for="' + id + '">&nbsp;</label>';
 		} else if(col.selectorType == 'range') {
 			var _val = [];
 			_val[0] = (value ? value[0] : col.defaultMinValue);
 			_val[1] = (value ? value[1] : col.defaultMaxValue);
-			this._selectors[col.name][line.name] = _val;
-			return '<div class="ci-rangebar-wrapper"><div class="ci-rangebar-min">' + Math.round((_val[0] * 100)) + ' %</div><div class="ci-rangebar" data-minvalue="' + col.minValue + '" data-maxvalue="' + col.maxValue + '" data-defaultmin="' + _val[0] + '" data-defaultmax="' + _val[1] + '" data-colid="' + col.name + '" data-lineid="' + line.name + '"></div><div class="ci-rangebar-max">' + (Math.round(_val[1] * 100)) + ' %</div></div>';
+
+			if(line)
+				this._selectors[col.name][line.name] = _val;
+			return '<div class="ci-rangebar-wrapper"><div class="ci-rangebar-min">' + Math.round((_val[0] * 100)) + ' %</div><div class="ci-rangebar" data-minvalue="' + col.minValue + '" data-maxvalue="' + col.maxValue + '" data-defaultmin="' + _val[0] + '" data-defaultmax="' + _val[1] + '" data-colid="' + colId + '" ' + (line ? ('data-lineid="' + lineId + '"') : '') + ' ></div><div class="ci-rangebar-max">' + (Math.round(_val[1] * 100)) + ' %</div></div>';
 		}
 	},
 
