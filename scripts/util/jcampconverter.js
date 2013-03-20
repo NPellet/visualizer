9c6c3c47633cd0b300d4164fd2fe21fd25317f39
Luc Patiny <luc@patiny.com>
2013-03-20 18:10:17 +0100

    Add spectra.contourLines for 2D Bruker NMR spectra

--- scripts/util/jcampconverter.js
+++ scripts/util/jcampconverter.js
@@ -28,6 +28,7 @@ CI.converter.jcampToSpectra=(function() {
 
 
         var spectra = [];
+        spectra.info={};
         var spectrum = {};
 
        // console.time("start");
@@ -49,7 +50,6 @@ CI.converter.jcampToSpectra=(function() {
             dataLabel=dataLabel.replace(/[_ -]/g,'').toUpperCase();
 
             if (dataLabel=='DATATABLE') {
-               
                 endLine=dataValue.indexOf("\n");
                 if (endLine==-1) endLine=dataValue.indexOf("\r");
                 if (endLine>0) {
@@ -69,7 +69,6 @@ CI.converter.jcampToSpectra=(function() {
 
                     if (xIndex==-1) xIndex=0;
                     if (yIndex==-1) yIndex=0;
-
 
                     if (ntuples.first) {
                         if (ntuples.first.length>xIndex) spectrum.firstX=ntuples.first[xIndex];
@@ -92,9 +91,6 @@ CI.converter.jcampToSpectra=(function() {
                     }
                     
                     spectrum.datatable=infos[0];
-
-
-
                     if (infos[1] && infos[1].indexOf("PEAKS")>-1) {
                         dataLabel="PEAKTABLE";
                     } else if (infos[1] && (infos[1].indexOf("XYDATA") || infos[0].indexOf("++")>0)) {
@@ -125,8 +121,10 @@ CI.converter.jcampToSpectra=(function() {
                 spectrum.yFactor = parseFloat(dataValue);
             } else if (dataLabel=='DELTAX') {
                 spectrum.deltaX = parseFloat(dataValue);
-            } else if (dataLabel=='.OBSERVEFREQUENCY') {
-                spectrum.observeFrequency=parseFloat(dataValue);
+            } else if (dataLabel=='.OBSERVEFREQUENCY' || dataLabel=='$SFO1') {
+                if (!spectrum.observeFrequency) spectrum.observeFrequency=parseFloat(dataValue);
+            } else if (dataLabel=='$SFO2') {
+                if (!spectra.indirectFrequency) spectra.indirectFrequency=parseFloat(dataValue);
             } else if (dataLabel=='$OFFSET') {   // OFFSET for Bruker spectra
                 shiftOffsetNum = 0;
                 shiftOffsetVal = parseFloat(dataValue);
@@ -161,6 +159,10 @@ CI.converter.jcampToSpectra=(function() {
                 ntuples.max=convertToFloatArray(dataValue.split(/[, \t]+/));
             } else if (dataLabel=='PAGE') {
                 spectrum.page=dataValue.trim();
+                spectrum.pageValue=parseFloat(dataValue.replace(/^.*=/,""));
+                if (spectra.indirectFrequency) {
+                    spectrum.pageValue/=spectra.indirectFrequency;
+                }
             } else if (dataLabel=="XYDATA") {
                 prepareSpectrum(spectrum);
                 parseXYData(spectrum, dataValue);
@@ -171,17 +173,16 @@ CI.converter.jcampToSpectra=(function() {
                 parsePeakTable(spectrum, dataValue);
                 spectra.push(spectrum);
                 spectrum={};
+            } else if (dataLabel.match(/^[A-Z].*/)) {
+                spectra.info[dataLabel]=dataValue.trim();
             }
         }
-       // console.timeEnd("start");
-      //  console.time("lowres");
+
         if (options && options.lowRes) addLowRes(spectra,options);
-       // console.timeEnd("lowres");
 
-       // console.log(spectra);
-
-     //  getNoiseLevel(spectra);
-      // add2D(spectra);
+        if (spectra.info.DATATYPE && (spectra.info.DATATYPE.indexOf("nD")>-1)) {
+            add2D(spectra);
+        }
 
         return spectra;
 
@@ -234,8 +235,10 @@ CI.converter.jcampToSpectra=(function() {
         var currentX = spectrum.firstX;
         var currentY = spectrum.firstY;
         var lines=value.split(/[\r\n]+/);
-        var lastDif, values, ascii;
+        var lastDif, values, ascii, expectedY;
+        values=[];
         for (var i=1, ii=lines.length; i<ii; i++) {
+            var previousValues=JSON.parse(JSON.stringify(values));
             values=lines[i].trim().replace(removeCommentRegExp,"").split(xyDataSplitRegExp);
             if (values.length>0) {
                 if (DEBUG) {
@@ -255,24 +258,48 @@ CI.converter.jcampToSpectra=(function() {
                     if (j==1 && (lastDif || lastDif==0)) {
                         lastDif = undefined; // at the beginning of each line there should be the full value X / Y so the diff is always undefined
                         // we could check if we have the expected Y value
+                        ascii=values[j].charCodeAt(0);
+                        
+                        if (false) { // this code is just to check the jcamp DIFDUP and the next line repeat of Y value
+                            // + - . 0 1 2 3 4 5 6 7 8 9
+                            if ((ascii==43) || (ascii==45) || (ascii==46) || ((ascii>47) && (ascii<58))) {
+                                expectedY=parseFloat(values[j]);
+                            } else
+                            // positive SQZ digits @ A B C D E F G H I (ascii 64-73)
+                            if ((ascii>63) && (ascii<74)) {
+                                expectedY=parseInt(String.fromCharCode(ascii-16)+values[j].substring(1));
+                            } else
+                            // negative SQZ digits a b c d e f g h i (ascii 97-105)
+                            if ((ascii>96) && (ascii<106)) {
+                                expectedY=- parseInt(String.fromCharCode(ascii-48)+values[j].substring(1));
+                           }
+                           if (expectedY!=currentY) {
+                                console.log("Y value check error: Found: "+expectedY+" - Current: "+currentY);
+                                console.log("Previous values: "+previousValues.length);
+                                console.log(previousValues);
+                           }
+                       }
                     } else {
                         if (values[j].length>0) {
                             ascii=values[j].charCodeAt(0);
                             // + - . 0 1 2 3 4 5 6 7 8 9
                             if ((ascii==43) || (ascii==45) || (ascii==46) || ((ascii>47) && (ascii<58))) {
+                                lastDif=undefined;
                                 currentY=parseFloat(values[j]);
                                 addPoint(spectrum,currentX,currentY);
                                 currentX+=spectrum.deltaX;
                             } else
                             // positive SQZ digits @ A B C D E F G H I (ascii 64-73)
                             if ((ascii>63) && (ascii<74)) {
+                                lastDif=undefined;
                                 currentY=parseInt(String.fromCharCode(ascii-16)+values[j].substring(1));
                                 addPoint(spectrum,currentX,currentY);
                                 currentX+=spectrum.deltaX;
                             } else
                             // negative SQZ digits a b c d e f g h i (ascii 97-105)
                             if ((ascii>96) && (ascii<106)) {
-                                currentY=- parseInt(String.fromCharCode(ascii-48)+values[j].substring(1));
+                                lastDif=undefined;
+                                currentY=-parseInt(String.fromCharCode(ascii-48)+values[j].substring(1));
                                 addPoint(spectrum,currentX,currentY);
                                 currentX+=spectrum.deltaX;
                            } else 
@@ -280,7 +307,7 @@ CI.converter.jcampToSpectra=(function() {
 
 
                             // DUP digits S T U V W X Y Z s (ascii 83-90, 115)
-                            if  ((ascii>82) && ((ascii==115) || (ascii<91))) {
+                            if  (((ascii>82) && (ascii<91)) || (ascii==115)) {
                                 var dup = parseInt(String.fromCharCode(ascii-34)+values[j].substring(1))-1;
                                 if (ascii==115) {
                                     dup = parseInt("9"+values[j].substring(1))-1;
@@ -315,8 +342,9 @@ CI.converter.jcampToSpectra=(function() {
                         }
                     }
                 }  
-            }  
+            }
         }
+
 
         function addPoint(spectrum,currentX,currentY) {
             // if (aa++<10) console.log(currentX+" - "+currentY+" - "+currentX/spectrum.observeFrequency+" - "+currentY*spectrum.yFactor);
@@ -329,26 +357,155 @@ CI.converter.jcampToSpectra=(function() {
         }
     }
 
-
-    function add2D(spectra) {
-
-    }
-
-    function getNoiseLevel(spectra) {
-        var averageAbsDifference = 0;
-        var nbPoints=0;
-        for (var i=0, ii=spectra.length; i<ii; i++) {
-            nbPoints+=spectra[i].data[0].length;
-            for (var j=1, jj=spectra[i].data[0].length; j<jj; j++) {
-                averageAbsDifference += Math.abs(spectra[i].data[0][j] - spectra[i].data[0][j-1]);
+    function convertTo3DZ(spectra) {
+        //console.time("ConvertTo3DZ");
+        var z=[];
+        var noise=0;
+        var minZ=spectra[0].data[0][0];
+        var maxZ=spectra[0].data[0][0];
+        var ySize=spectra.length;
+        var xSize=spectra[0].data[0].length/2;
+        for (var i=0; i<ySize; i++) {
+            z[i]=[];
+            for (var j=0; j<xSize; j++) {
+                z[i][j]=spectra[i].data[0][j*2+1];
+                if (z[i][j]<minZ) minZ=spectra[i].data[0][j*2+1];
+                if (z[i][j]>maxZ) maxZ=spectra[i].data[0][j*2+1];
+                if (i!=0 && j!=0) {
+                    noise += Math.abs(z[i][j] - z[i][j-1])+Math.abs(z[i][j] - z[i-1][j]);
+                }
             }
         }
-        averageAbsDifference/=nbPoints;
-        console.log(averageAbsDifference);
-
-        return averageAbsDifference;
+        //console.timeEnd("ConvertTo3DZ");
+        return {
+            z: z, 
+            minX: spectra[0].data[0][0], 
+            maxX: spectra[0].data[0][spectra[0].data[0].length-2],
+            minY: spectra[0].pageValue, 
+            maxY: spectra[spectra.length-1].pageValue,
+            minZ: minZ,
+            maxZ: maxZ,
+            noise: noise/((z.length-1)*(z[0].length-1)*2)
+        };
+       
     }
 
+    function add2D(spectra) {
+        var zData=convertTo3DZ(spectra);
+        spectra.contourLines=generateContourLines(zData);
+        delete zData.z;
+        spectra.minMax=zData;
+    }
+
+ 
+
+    function generateContourLines(zData) {
+        //console.time("generateContourLines");
+        var noise=zData.noise;
+        var z=zData.z;
+        var contourLevels=[];
+        var nbLevels=7;
+        var povarHeight = new Float32Array(4);
+        var isOver = [];
+        var nbSubSpectra=z.length;
+        var nbPovars=z[0].length;
+        var pAx, pAy, pBx, pBy;
+
+        var x0 = zData.minX;
+        var xN = zData.maxX;
+        var dx = (xN - x0) / (nbPovars-1);
+        var y0 = zData.minY;
+        var yN = zData.maxY;
+        var dy = (yN - y0) / (nbSubSpectra-1);
+        var minZ=zData.minZ;
+        var maxZ=zData.maxZ;
+
+        //System.out.prvarln("y0 "+y0+" yN "+yN);
+        // -------------------------
+        // Povars attribution
+        //
+        // 0----1
+        // |  / |
+        // | /  |
+        // 2----3
+        //
+        // ---------------------d------
+
+        var lineZValue;
+        for (var level = 0; level < nbLevels*2; level++) { // multiply by 2 for positif and negatif
+            var contourLevel={};
+            contourLevels.push(contourLevel);
+            var side=level%2;
+            if (side==0) {
+                lineZValue=(maxZ - 5 * noise) * Math.exp(level/2 - nbLevels) + 5 * noise;
+            } else {
+                lineZValue=-(maxZ - 5 * noise) * Math.exp(level/2 - nbLevels) - 5 * noise;
+            }
+            var lines=[];
+            contourLevel.zValue=lineZValue;
+            contourLevel.lines=lines;
+
+            if (lineZValue <= minZ || lineZValue >= maxZ) continue;
+            
+            for (var iSubSpectra = 0; iSubSpectra < nbSubSpectra - 1; iSubSpectra++) {
+                for (var povar = 0; povar < nbPovars - 1; povar++) {
+                    povarHeight[0] = z[iSubSpectra][povar];
+                    povarHeight[1] = z[iSubSpectra][povar + 1];
+                    povarHeight[2] = z[(iSubSpectra + 1)][povar];
+                    povarHeight[3] = z[(iSubSpectra + 1)][(povar + 1)];
+
+                    for (var i = 0; i < 4; i++) {
+                        isOver[i] = (povarHeight[i] > lineZValue);
+                    }
+
+                    // Example povar0 is over the plane and povar1 and
+                    // povar2 are below, we find the varersections and add
+                    // the segment
+                    if (isOver[0] != isOver[1] && isOver[0] != isOver[2]) {
+                        pAx = povar + (lineZValue - povarHeight[0]) / (povarHeight[1] - povarHeight[0]);
+                        pAy = iSubSpectra;
+                        pBx = povar;
+                        pBy = iSubSpectra + (lineZValue - povarHeight[0]) / (povarHeight[2] - povarHeight[0]);
+                        lines.push(pAx * dx + x0, pAy * dy + y0, pBx * dx + x0, pBy * dy + y0);
+                    }
+                    if (isOver[3] != isOver[1] && isOver[3] != isOver[2]) {
+                        pAx = povar + 1;
+                        pAy = iSubSpectra + 1 - (lineZValue - povarHeight[3]) / (povarHeight[1] - povarHeight[3]);
+                        pBx = povar + 1 - (lineZValue - povarHeight[3]) / (povarHeight[2] - povarHeight[3]);
+                        pBy = iSubSpectra + 1;
+                        lines.push(pAx * dx + x0, pAy * dy + y0, pBx * dx + x0, pBy * dy + y0);
+                    }
+                    // test around the diagonal
+                    if (isOver[1] != isOver[2]) {
+                        pAx = povar + 1 - (lineZValue - povarHeight[1]) / (povarHeight[2] - povarHeight[1]);
+                        pAy = iSubSpectra + (lineZValue - povarHeight[1]) / (povarHeight[2] - povarHeight[1]);
+                        if (isOver[1] != isOver[0]) {
+                            pBx = povar + 1 - (lineZValue - povarHeight[1]) / (povarHeight[0] - povarHeight[1]);
+                            pBy = iSubSpectra;
+                            lines.push(pAx * dx + x0, pAy * dy + y0, pBx * dx + x0, pBy * dy + y0);
+                        }
+                        if (isOver[2] != isOver[0]) {
+                            pBx = povar;
+                            pBy = iSubSpectra + 1 - (lineZValue - povarHeight[2]) / (povarHeight[0] - povarHeight[2]);
+                            lines.push(pAx * dx + x0, pAy * dy + y0, pBx * dx + x0, pBy * dy + y0);
+                        }
+                        if (isOver[1] != isOver[3]) {
+                            pBx = povar + 1;
+                            pBy = iSubSpectra + (lineZValue - povarHeight[1]) / (povarHeight[3] - povarHeight[1]);
+                            lines.push(pAx * dx + x0, pAy * dy + y0, pBx * dx + x0, pBy * dy + y0);
+                        }
+                        if (isOver[2] != isOver[3]) {
+                            pBx = povar + (lineZValue - povarHeight[2]) / (povarHeight[3] - povarHeight[2]);
+                            pBy = iSubSpectra + 1;
+                            lines.push(pAx * dx + x0, pAy * dy + y0, pBx * dx + x0, pBy * dy + y0);
+                        }
+                    }
+                }
+            }
+        }
+        // console.timeEnd("generateContourLines");
+        return contourLevels;
+    }
 
 
     function addLowRes(spectra, options) {
