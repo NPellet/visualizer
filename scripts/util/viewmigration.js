b7b41180880a24677b434c8c52f9661f165433f8
Luc Patiny <luc@patiny.com>
2013-09-23 12:58:57 +0200

    Resize module if change of version

--- scripts/util/viewmigration.js
+++ scripts/util/viewmigration.js
@@ -10,6 +10,16 @@ define(['jquery', 'util/versioning'], function($, Versioning) {
 				if(view.entryPoint) {
 					view.variables = view.entryPoint.variables;
 					delete view.entryPoint;
+
+					-				// we should also resize the modules
+	-				var modules=view.modules;
+	-				for (var i=0; i<modules.length; i++) {
+	-					var module=modules[i];
+	-					module.position.left*=2;
+	-					module.position.top*=2;
+	-					module.size.width*=2;
+	-					module.size.height*=2;
+	-				}
 				}
 			break;
 		}
