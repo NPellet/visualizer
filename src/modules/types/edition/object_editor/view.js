1c833909fc6970fbe3ccac4ea0eb332af177b974
Michaël Zasso <mic.besace@gmail.com>
2014-01-17 08:17:07 +0100

    move context listener

--- src/modules/types/edition/object_editor/view.js
+++ src/modules/types/edition/object_editor/view.js
@@ -10,34 +10,36 @@ define(['modules/default/defaultview', "src/util/util", "components/jsoneditor/j
     view.prototype = $.extend(true, {}, Default, {
         init: function() {
             var that = this;
-            var html = '<div id="' + this._id + '"></div>';
-
-            this.dom = $(html).css({
-                height: '100%',
-                width: '100%'
-            });
-
-            this.module.getDomContent( ).html(this.dom);
-
-            Context.listen(this.module.getDomWrapper().get(0), [], function(contextDom) {
-                var li = $('<li><a>Change editor type</a></li>');
-
-                var ul = $("<ul/>").appendTo(li);
-                
-                var elements={"Read":"view","Edit":"tree","Text":"text"};
-                for (var i in elements) {
-                    var li2 = $("<li>").appendTo(ul);
-                    li2.html('<a>'+i+'</a>');
-                }
-                $(contextDom).append(li);
-                li.bind('click', function(event) {
-                    that.editor.setMode(elements[event.target.text]);
+            if (!this.dom) {
+                this.dom = $('<div id="' + this._id + '"></div>').css({
+                    height: '100%',
+                    width: '100%'
                 });
-            });
+                this.module.getDomContent( ).html(this.dom);
+                Context.listen(this.module.getDomWrapper().get(0), [], function(contextDom) {
+                    var li = $('<li><a>Change editor type</a></li>');
+
+                    var ul = $("<ul/>").appendTo(li);
+
+                    var elements = {"Read": "view", "Edit": "tree", "Text": "text"};
+                    for (var i in elements) {
+                        var li2 = $("<li>").appendTo(ul);
+                        li2.html('<a>' + i + '</a>');
+                    }
+                    $(contextDom).append(li);
+                    li.bind('click', function(event) {
+                        var mode = elements[event.target.text];
+                        that.editor.setMode(mode);
+			//that.module.getConfiguration('editable')[0] = mode; NEED TO UPDATE CONFIG
+                    });
+                });
+            }
+
             this.onReady = $.Deferred();
         },
         blank: {},
         inDom: function() {
+            this.dom.empty();
             var mode = this.module.getConfiguration('editable');
             this.expand = this.module.getConfiguration('expanded') || false;
             this.editor = new jsoneditor.JSONEditor(document.getElementById(this._id), {mode: mode, change: this.editorChanged, module: this.module});
@@ -46,7 +48,8 @@ define(['modules/default/defaultview', "src/util/util", "components/jsoneditor/j
         update: {
             value: function(value) {
                 this.editor.set(value.get());
-                if(this.expand[0]) this.editor.expandAll();
+                if (this.expand[0])
+                    this.editor.expandAll();
             }
         },
         editorChanged: function() {
