d25f6c4bc605c035d61784edc401bf5eff016dd0
Michaël Zasso <mic.besace@gmail.com>
2014-01-24 10:49:15 +0100

    add test

--- src/modules/types/chart/statistics/parallel_coordinates/view.js
+++ src/modules/types/chart/statistics/parallel_coordinates/view.js
@@ -21,18 +21,27 @@ define(['modules/default/defaultview', "src/util/util","lib/parallel-coordinates
         },
         blank: {},
         inDom: function() {
-            this.graph = d3.parcoords()("#"+(this._id));
-            var foods = [
-  {name: "Asparagus", protein: 2.2, calcium: 0.024, sodium: 0.002},
-  {name: "Butter", protein: 0.85, calcium: 0.024, sodium: 0.714},
-  {name: "Coffeecake", protein: 6.8, calcium: 0.054, sodium: 0.351},
-  {name: "Pork", protein: 28.5, calcium: 0.016, sodium: 0.056},
-  {name: "Provolone", protein: 25.58, calcium: 0.756, sodium: 0.876}
-];
-this.graph.data(foods);
-//this.graph.dimensions(["protein","calcium"]);
-window.graph = this.graph;
-this.graph.render();
+            var paracoords = d3.parcoords()("#"+(this._id));
+            /*var foods = [
+                {name: "Asparagus", protein: 2.2, calcium: 0.024, sodium: 0.002},
+                {name: "Butter", protein: 0.85, calcium: 0.024, sodium: 0.714},
+                {name: "Coffeecake", protein: 6.8, calcium: 0.054, sodium: 0.351},
+                {name: "Pork", protein: 28.5, calcium: 0.016, sodium: 0.056},
+                {name: "Provolone", protein: 25.58, calcium: 0.756, sodium: 0.876}
+            ];*/
+            var cube = [
+                [0, 0, 0],
+                [1, 0, 0],
+                [0, 1, 0],
+                [1, 1, 0],
+                [0, 0, 1],
+                [1, 0, 1],
+                [0, 1, 1],
+                [1, 1, 1]
+            ];
+            paracoords.data(cube);
+            //paracoords.dimensions([1,2]);
+            paracoords.render().ticks(2).createAxes();
             this.onReady.resolve();
         },
         update: {
