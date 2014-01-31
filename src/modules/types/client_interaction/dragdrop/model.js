ce1ae7fa7c0cf7b41144c4609fb18cb1dd61e0f0
Michaël Zasso <mic.besace@gmail.com>
2014-01-31 11:23:38 +0100

    update drag n drop

--- src/modules/types/client_interaction/dragdrop/model.js
+++ src/modules/types/client_interaction/dragdrop/model.js
@@ -12,6 +12,17 @@ define(['modules/default/defaultmodel','src/util/datatraversing'], function(Defa
 		
 		getjPath: function(rel, accepts) {
                     var jpaths = [];
+                    
+                    // Populate tmpVars with empty object so the user can set a variable out even if no file was dropped
+                    var definedDrops;
+                    if(definedDrops = this.module.getConfiguration("vars")) {
+                        for(var i = 0; i < definedDrops.length; i++) {
+                            var def = definedDrops[i];
+                            if(!this.tmpVars.hasOwnProperty(def.variable)) {
+                                this.tmpVars[def.variable] = new DataObject();
+                            }
+                        }
+                    }
                     return Traversing.getJPathsFromElement(this.tmpVars, jpaths), jpaths;
 		}
 	});
