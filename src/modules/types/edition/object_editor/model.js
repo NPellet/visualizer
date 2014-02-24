01d22807bdb5f0bd78207ddad31713156b08ac8e
mzasso <mzasso@cdm8-217.epfl.ch>
2014-02-24 17:23:11 +0100

    Add option to store object in view fix #78 fix #79

--- src/modules/types/edition/object_editor/model.js
+++ src/modules/types/edition/object_editor/model.js
@@ -1,8 +1,10 @@
-define(['modules/default/defaultmodel'], function(Default) {
+define(['modules/default/defaultmodel', 'src/util/datatraversing'], function(Default, Traversing) {
 	function model() {};
 	model.prototype = $.extend(true, {}, Default, {
-            getjPath : function() {
-                return [];
+            getjPath: function(rel) {
+                var jpaths = [];
+                Traversing.getJPathsFromElement(this.module.view._data, jpaths);
+                return jpaths;
             }
 	});
 
