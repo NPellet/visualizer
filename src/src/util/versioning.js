2e14f1f0bf88370504c5a2d966819fdf6bcf54b6
Michaël Zasso <mic.besace@gmail.com>
2014-03-25 16:53:03 +0100

    use URI and update versionloader

--- src/src/util/versioning.js
+++ src/src/util/versioning.js
@@ -26,46 +26,28 @@ define(['src/util/versionhandler'], function(VersionHandler) {
             }
         };
         
-        function switchView(value, pushstate) {
-            require(["components/uri.js/src/URI"],function(URI){
-                var def;
-                if(value.data) {
-                    def = setData(value.data.url, value.data.branch);
-                } else {
-                    def = $.Deferred().resolve();
-                }
-                if(value.view) {
-                    def.done(function(){
-                        setView(value.view.url, value.view.branch);
-                    });
-                }
-                if(pushstate) {
-                    var location = window.location,
-                        search = location.search.split("&");
-                    var hasView = false, hasData = false, hasElems = false;
-                    if(location.search.length > 0)
-                        hasElems = true;
-                    for(var i = 0; i < search.length; i++) {
-                        var str = search[i];
-                        if(str.indexOf("dataURL=")!==-1 && value.data) {
-                            search[i] = str.replace(/dataURL=.*/,"dataURL="+value.data.url);
-                            hasData = true;
-                        } else if(str.indexOf("viewURL=")!==-1 && value.view) {
-                            search[i] = str.replace(/viewURL=.*/,"viewURL="+value.view.url);
-                            hasView = true;
-                        }
-                    }
-                    search = search.join("&");
-                    if(!hasElems)
-                        search += "?";
-                    if(!hasData && value.data)
-                        search += "dataURL="+value.data.url+"&";
-                    if(!hasView && value.view)
-                        search += "viewURL="+value.view.url+"&";
-
-                    window.history.pushState({type:"viewchange",value:value}, "", location.origin+location.pathname+search);
-                }
-            });
+        function switchView(value, pushstate) {console.log("VIEW SWITCHED")
+            var def;
+            if(value.data) {
+                def = setData(value.data.url, value.data.branch);
+            } else {
+                def = $.Deferred().resolve();
+            }
+            if(value.view) {
+                def.done(function(){
+                    setView(value.view.url, value.view.branch);
+                });
+            }
+            if(pushstate) {
+                require(["components/uri.js/src/URI"],function(URI){
+                    var uri = new URI(window.location.href);
+                    if(value.data)
+                        uri.setQuery("dataURL",value.data.url);
+                    if(value.view)
+                        uri.setQuery("viewURL", value.view.url);
+                    window.history.pushState({type:"viewchange",value:value}, "", uri.href());
+                });
+            }
         }
         
         function setView(url, branch, defUrl) {
