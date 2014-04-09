c496ac730b30e77ebca171c651880ba8cccddfd2
Luc Patiny <luc@patiny.com>
2014-04-09 16:45:46 +0200

    Generate event only if there is a value change

--- src/modules/types/client_interaction/paste_text/view.js
+++ src/modules/types/client_interaction/paste_text/view.js
@@ -2,7 +2,7 @@ define(['modules/default/defaultview','src/util/api'], function(Default, API) {
 	
 	function view() {};
 	view.prototype = $.extend(true, {}, Default, {
-
+		currentValue: "",
 		init: function() {	
 			var html = "";
 			html += '<div></div>';
@@ -21,9 +21,13 @@ define(['modules/default/defaultview','src/util/api'], function(Default, API) {
                 "width":"99%",
                 "height":"99%"
             }).on("keyup", function() {
-                that.module.controller.valueChanged(textarea.val());
+            	if (that.value!=textarea.val()) {
+            		that.module.controller.valueChanged(textarea.val());
+            		that.value=textarea.val();
+            	}
             }).val(this.module.getConfiguration("thevalue"));
             this.dom.append(textarea);
+            that.value=textarea.val();
             this.module.controller.valueChanged(textarea.val());
         }
 	});
