11256d25eba42b78ca6cb35f86b45ef4de274828
NPellet <norman.pellet@gmail.com>
2014-05-04 23:27:24 +0200

    Fix money

--- src/src/util/util.js
+++ src/src/util/util.js
@@ -308,7 +308,20 @@ define([], function() {
 				{title: "Trebuchet MS", key: "'Trebuchet MS'"},
 				{title: "Verdana", key: "Verdana"}
 			];
+		},
+
+		// http://stackoverflow.com/questions/9318674/javascript-number-currency-formatting
+		formatMoney: function(n, decPlaces, thouSeparator, decSeparator) {
+		    
+		    decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
+		    decSeparator = decSeparator == undefined ? "." : decSeparator,
+		    thouSeparator = thouSeparator == undefined ? "," : thouSeparator,
+		    sign = n < 0 ? "-" : "",
+		    i = parseInt(n = Math.abs(+n || 0).toFixed(decPlaces)) + "",
+		    j = (j = i.length) > 3 ? j % 3 : 0;
+		    return sign + (j ? i.substr(0, j) + thouSeparator : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thouSeparator) + (decPlaces ? decSeparator + Math.abs(n - i).toFixed(decPlaces).slice(2) : "");
 		}
+
 	};
 
 });
