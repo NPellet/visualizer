899d1e5700273eda23231b4b493cfbbf56c3da24
Michaël Zasso <mic.besace@gmail.com>
2014-05-13 16:18:04 +0200

    fix postit content saving and reloading

--- src/modules/types/display/postit/view.js
+++ src/modules/types/display/postit/view.js
@@ -14,21 +14,21 @@ define(['modules/default/defaultview', 'forms/button', 'src/util/util', 'src/mai
 			this.dom = $('<div />', {  class: 'postit' }).css("font-family", this.module.getConfiguration("fontfamily")+", Arial");
 			this.inside = $('<div>', { id: id, class: 'inside', contentEditable: 'true' }).bind('keyup', function() {
 
-				self.module.definition.text = $(this).html();
+				if(self.instance) self.module.definition.text = self.instance.getData();
 				self.module.getDomWrapper().height($(this).height() + 70);
 				Grid.moduleResize(self.module);
 
 				//console.log($(this).html());
-			}).html(self.module.definition.text || '').bind('focus', function() {
-				require(['ckeditor'], function(CKEDITOR) {
-					if(done)
-						return;
-					CKEDITOR.disableAutoInline = true;
-					CKEDITOR.inline(self._id, {
-						extraPlugins:"mathjax"
-					});
-					done = true;
+			}).html(self.module.definition.text || '');
+			
+			require(['ckeditor'], function(CKEDITOR) {
+				if(done)
+					return;
+				CKEDITOR.disableAutoInline = true;
+				self.instance = CKEDITOR.inline(self._id, {
+					extraPlugins:"mathjax"
 				});
+				done = true;
 			});
 
 			this.dom.html(this.inside);
