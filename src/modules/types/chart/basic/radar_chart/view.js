34d3d0598ea480b803f59428292576a0f988df8c
Arroub Khalid <azepor@gmail.com>
2014-05-24 02:00:46 +0000

    RadarChart: fixing hightlight bug

--- src/modules/types/chart/basic/radar_chart/view.js
+++ src/modules/types/chart/basic/radar_chart/view.js
@@ -10,25 +10,13 @@ define(['modules/default/defaultview','src/util/datatraversing','src/util/api','
 		init: function() {
 
 			if (this.DEBUG) console.log("Radar Chart: init");
-			if (this.dom) {
-				// in the dom exists and the preferences has been changed we need to clean the canvas
-				this.dom.empty();
 
-			}
 			// When we change configuration the method init is called again. Also the case when we change completely of view
 			if (! this.dom) {
 				this._id = Util.getNextUniqueId();
 				this.dom = $('<div style="height: 100%;width: 100%" id="' + this._id + '"></div>');
 				this.module.getDomContent().html(this.dom);
 			}
-			// if the dom existed there was probably a graph or when changing of view
-			if (this._radar) { 
-				delete this._radar;
-			}
-
-			// Adding a deferred allows to wait to get actually the data before we draw the chart
-			// we decided here to plot the chart in the "onResize" event
-			this.loadedData=$.Deferred();
 
 			if (this.DEBUG) console.log("Radar Chart: ID: "+this._id);
 
@@ -45,41 +33,8 @@ define(['modules/default/defaultview','src/util/datatraversing','src/util/api','
 		},
 
 		onResize: function() {
-
 			if (this.DEBUG) console.log("Radar Chart: onResize");
-
-			var self=this;
-
-			this.loadedData.done(function() {
-				self._radar.parse(self._data,"json");
-
-				self._radar.attachEvent("onMouseMove", function (id, ev, trg)
-				{
-					self._data.forEach(function(entry) 
-					{
-					
-						if(entry.id == id)
-						{
-							var obj = entry;
-							if(ev.toElement.outerHTML[ev.toElement.outerHTML.length -3] == 'd')
-						{
-							self.module.controller.elementHover(obj._highlight[0]);
-						}
-						else
-						{
-							self.module.controller.elementHover(obj._highlight[ev.toElement.outerHTML[ev.toElement.outerHTML.length -3]]);
-						}}
-
-
-					});		
-					return true;
-
-				}); 
-				self._radar.attachEvent("onMouseOut", function (id, ev, trg){
-							self.module.controller.elementOut();
-				}); 
-			});
-
+			this._redraw();
 		},
 
 		/* When a value change this method is called. It will be called for all 
@@ -87,50 +42,98 @@ define(['modules/default/defaultview','src/util/datatraversing','src/util/api','
 		It will also be called at the beginning and in this case the value is null !
 		*/
 		update: {
-
 			'chart': function(moduleValue) 
 			{
-				var self=this;
-				var cfg = $.proxy( this.module.getConfiguration, this.module );
-
 				if (this.DEBUG) console.log("Radar Chart: update from chart object");
 
-					if (! moduleValue || ! moduleValue.value) return;
+				if (! moduleValue) {
+					delete this.value;
+				} else {
+					this.value=moduleValue.get();
+				}
 
-					this.updateOptions(cfg, moduleValue.get());
-
-					this._convertChartToData(moduleValue.get());
-
-					this.loadedData.resolve();
+				this._redraw();
 			},
 
 
 		},
 
-		_convertChartToData: function(value,radar) {
+
+		_redraw: function() {
+			if (! this.value) {
+				return;
+			}
+
+			if (this.dom) {
+				this.dom.empty();
+			}
+
+			// Redrawing the chart requires 3 steps
+			// 1. convert the data
+			// 2. create an empty chart
+			// 3. apply the data
+
+			var data=this._convertChartToData(this.value);
+
+
+			this.createChart(this.value);
+
+			this._radar.parse(data,"json");
+
 			var self=this;
-
-			if ( ! value.data instanceof Array || ! value ) return;
-
-			for (var j = 0; j < value.data[0].x.length; j++) 
+			this._radar.attachEvent("onMouseMove", function (id, ev, trg)
 			{
-				self._data[j] = {};
-				self._data[j]["xunit"] = value.data[0].x[j];
-				self._data[j]['_highlight'] = [];	
-				for (var i = 0; i < value.data.length; i++) 
+				data.forEach(function(entry) 
 				{
-					self._data[j][value.data[i].name] = value.data[i].y[j];
-					self._data[j]['_highlight'].push({name: value.data[i].name, _highlight: value.data[i]._highlight[j]});
 
-				}
+					if(entry.id == id)
+					{
+						var obj = entry;
+						if(ev.toElement.outerHTML[ev.toElement.outerHTML.length -3] == 'd')
+					{
+						self.module.controller.elementHover(obj._highlight[0]);
+					}
+					else
+					{
+						self.module.controller.elementHover(obj._highlight[ev.toElement.outerHTML[ev.toElement.outerHTML.length -3]]);
+					}}
 
-			};
-			
-			
+
+				});		
+				return true;
+
+			}); 
+			this._radar.attachEvent("onMouseOut", function (id, ev, trg){
+						self.module.controller.elementOut();
+			}); 
+
+
+
 		},
 
-		updateOptions: function(cfg, chart) {
+		_convertChartToData: function(value) {
+			var data=[];
+			if (value && value.data instanceof Array) {
+				for (var j = 0; j < value.data[0].x.length; j++) 
+				{
+					data[j] = {};
+					data[j]["xunit"] = value.data[0].x[j];
+					data[j]['_highlight'] = [];	
+					for (var i = 0; i < value.data.length; i++) 
+					{
+						data[j][value.data[i].name] = value.data[i].y[j];
+						if (value.data[i]._highlight && value.data[i]._highlight[j]) {
+							data[j]['_highlight'].push({name: value.data[i].name, _highlight: value.data[i]._highlight[j]});
+						}
+					}
+				};
+			}
+			return data;
+		},
+
+		createChart: function(chart, data) {
 			var self=this;
+			var cfg = $.proxy( this.module.getConfiguration, this.module );
 			switch (cfg('preference'))
 			{
 			case 'radar':
@@ -143,9 +146,9 @@ define(['modules/default/defaultview','src/util/datatraversing','src/util/api','
 					color: chart.data[0].color,
 					fill: chart.data[0].color,
 					line:{
-								color:chart.data[0].color,
-								width:1
-							},
+						color:chart.data[0].color,
+						width:1
+					},
 					xAxis:{
 							template:"#xunit#"
 					},
@@ -162,21 +165,21 @@ define(['modules/default/defaultview','src/util/datatraversing','src/util/api','
 				var val = []
 
 				for (var i = 0; i < chart.data.length; i++) 
+					{
+						if(i != 0)
 						{
-							if(i != 0)
-							{
-							this._radar.addSeries({
-									value: "#"+chart.data[i].name+"#",
-									fill: chart.data[i].color,
-									line:{
-										color:chart.data[i].color,
-										width:1
-									},
+						this._radar.addSeries({
+								value: "#"+chart.data[i].name+"#",
+								fill: chart.data[i].color,
+								line:{
+									color:chart.data[i].color,
+									width:1
+								},
 
-							})
-							}
-							val.push({text: chart.data[i].serieLabel,color: chart.data[i].color});
+						})
 						}
+						val.push({text: chart.data[i].serieLabel,color: chart.data[i].color});
+					}
 				this._radar.define("legend",{
 					width: 120,
 					align: "left",
@@ -188,20 +191,18 @@ define(['modules/default/defaultview','src/util/datatraversing','src/util/api','
 					values: val
 				}); 
 				break;
-				
+
 			case 'pie':
-			var options = {
-			view: cfg('pie'),
-			container: self._id,
-			value: "#"+chart.data[0].name+"#",
-			color: chart.data[0].color,
+				var options = {
+					// view: cfg('pie'),
+					container: self._id,
+					value: "#"+chart.data[0].name+"#",
+					color: chart.data[0].color,
+					pieInnerText: "<b>#xunit#</b>"
+				};
+				this._radar = new dhtmlXChart(options);
 
-			pieInnerText: "<b>#xunit#</b>"
-
-			};
-			this._radar = new dhtmlXChart(options);
-
-			break;
+				break;
 			};
 
 
