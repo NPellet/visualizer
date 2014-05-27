a77995617a3ee582f5e1f587fc857907a47cd340
Daniel Kostro <kostro.d@gmail.com>
2014-05-27 10:47:21 +0200

    Ext req accept GET

--- testcase/externalReq/index.php
+++ testcase/externalReq/index.php
@@ -12,32 +12,60 @@
       molfile: '2e853f5d3e21623f28d235a412e1e169'
     }
     
-    var jsonval = "<?php echo $_POST['value'] ?>";
-    var name = "<?php echo $_POST['name'] ?>";
+    var data = {};
+    try {
+      <?php foreach($_POST as $key=>$postvar): ?>
+      console.log("post var: <?php echo $key ?>", "<?php echo $postvar ?>");
+      try {
+        data["<?php echo $key ?>"] = JSON.parse("<?php echo $postvar ?>")
+      } catch(e) {
+        data["<?php echo $key ?>"] = "<?php echo $postvar ?>";
+      }
+      <?php endforeach; ?>
+    } catch(e) {
+      console.log("went wrong");
+    }
     
-    if(!types[name]) {
-      writeBody('Error: '+name+' has no corresponding type');
+    try {
+      <?php foreach($_GET as $key=>$postvar): ?>
+      console.log("post var: <?php echo $key ?>", "<?php echo $postvar ?>");
+      try {
+        data["<?php echo $key ?>"] = JSON.parse("<?php echo $postvar ?>")
+      } catch(e) {
+        data["<?php echo $key ?>"] = "<?php echo $postvar ?>";
+      }
+      <?php endforeach; ?>
+    } catch(e) {
+      console.log("went wrong");
+    }
+    
+    
+    
+    console.log(data);
+        
+    if(!data.name || typeof data.name !== "string" || !types[data.name] || !data.value) {
+      writeBody('Error: '+data.name+' has no corresponding type');
     }
     else {
       var db = new PouchDB('external_infos');
       var rev = null;
     
-      db.get(name, function(err, res) {
+      db.get(data.name, function(err, res) {
         if(!err) {
           rev = res._rev
         }
         db.put({
-          _id: name,
+          _id: data.name,
           _rev: rev ? rev : undefined,
-          type: types[name],
-          value: jsonval,
-          views: views[name]
+          type: types[data.name],
+          value: data.value,
+          views: views[data.name]
         }, function(err, res) {
           if(!err) {
             db.compact();
             writeBody('Document written to database. Redirecting...');
             setTimeout(function() {
-               window.location = '/visualizer/_design/visualizer_head/index.html?config=default.json&viewURL=/c/'+views[name]+'/view.json'
+               //window.location = '/visualizer/_design/visualizer_head/index.html?config=default.json&viewURL=/c/'+views[data.name]+'/view.json'
             }, 1000);
           }
           else {
