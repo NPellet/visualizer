62727aa4cca6304fd9ed4160b40088ca283c0a54
Daniel Kostro <kostro.d@gmail.com>
2014-07-21 13:35:02 +0200

    Rich text use createDataFromEvent

--- src/modules/types/edition/rich_text/controller.js
+++ src/modules/types/edition/rich_text/controller.js
@@ -1,37 +1,37 @@
 define(['modules/default/defaultcontroller'], function(Default) {
 
-	function controller() {
-	}
-	;
+    function controller() {
+    }
+    ;
 
-	controller.prototype = $.extend(true, {}, Default);
+    controller.prototype = $.extend(true, {}, Default);
 
-	controller.prototype.moduleInformation = {
-		moduleName: 'Rich text editor',
-		description: 'Edit rich text in a wysiwyg interface.',
-		author: 'Michaël Zasso',
-		date: '21.05.2014',
-		license: 'MIT',
-		cssClass: 'rich_text'
-	};
+    controller.prototype.moduleInformation = {
+        moduleName: 'Rich text editor',
+        description: 'Edit rich text in a wysiwyg interface.',
+        author: 'Michaël Zasso',
+        date: '21.05.2014',
+        license: 'MIT',
+        cssClass: 'rich_text'
+    };
 
-	controller.prototype.references = {
-		html: {
-			label: 'Content as HTML'
-		}
-	};
+    controller.prototype.references = {
+        html: {
+            label: 'Content as HTML'
+        }
+    };
 
-	controller.prototype.events = {
-		onEditorChange: {
-			label: 'The value in the editor has changed',
-			refVariable: ['html']
-		}
-	};
+    controller.prototype.events = {
+        onEditorChange: {
+            label: 'The value in the editor has changed',
+            refVariable: ['html']
+        }
+    };
 
-	controller.prototype.valueChanged = function(value) {
-			this.module.definition.richtext = value;
-		this.setVarFromEvent('onEditorChange',new DataObject({type:"html",value:value}));
-	};
+    controller.prototype.valueChanged = function(value) {
+        this.module.definition.richtext = value;
+        this.createDataFromEvent("onEditorChange", "html", DataObject.check({type:"html", value: value}, true));
+    };
 
-	return controller;
+    return controller;
 });
