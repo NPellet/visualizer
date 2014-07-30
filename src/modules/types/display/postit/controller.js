a43bea6006e3ea43ac09de31ee1cda75144ad8d0
Daniel Kostro <kostro.d@gmail.com>
2014-07-30 15:08:16 +0200

    Update postit: editable option

--- src/modules/types/display/postit/controller.js
+++ src/modules/types/display/postit/controller.js
@@ -67,30 +67,40 @@ define( [ 'modules/default/defaultcontroller', 'src/util/util' ], function( Defa
 	};
 	
 		
-	controller.prototype.configurationStructure = function(section) {
-            var standardFonts = Util.getWebsafeFonts();
-            standardFonts.push({ title: "Post-it", key: "Post_IT"});
-		return { groups:{
-                        group:{
-                            options:{
-                                type:'list'
-                            },
-                            fields: {
-                                fontfamily:{
-                                    type:'combo',
-                                    title:'Font-family',
-                                    'default': 'Post_IT',
-                                    options: standardFonts
-                                }
-                            }
+    controller.prototype.configurationStructure = function(section) {
+        var standardFonts = Util.getWebsafeFonts();
+        standardFonts.push({ title: "Post-it", key: "Post_IT"});
+        return { 
+            groups: {
+                group: {
+                    options: {
+                        type:'list'
+                    },
+                    fields:  {
+                        fontfamily: {
+                            type:'combo',
+                            title:'Font-family',
+                            'default': 'Post_IT',
+                            options: standardFonts
+                        },
+                                
+                        editable: {
+                            type: 'checkbox',
+                            title: 'Is Editable',
+                            options: {isEditable: 'Yes'},
+                            default: ['isEditable']
                         }
-                } };
-	};
+                    }
+                }
+            }
+        };
+    };
 
 		
-	controller.prototype.configAliases = {
-            fontfamily:['groups','group',0,'fontfamily',0]
-        };
+    controller.prototype.configAliases = {
+        fontfamily:['groups','group',0,'fontfamily',0],
+        editable: [ 'groups', 'group', 0, 'editable', 0 ]
+    };
 
  	return controller;
 });
--- src/modules/types/display/postit/view.js
+++ src/modules/types/display/postit/view.js
@@ -10,28 +10,34 @@ define(['modules/default/defaultview', 'forms/button', 'src/util/util', 'src/mai
 				done = false;
 
 			this._id = id;
-
-			this.dom = $('<div />', {  class: 'postit' }).css("font-family", this.module.getConfiguration("fontfamily")+", Arial");
-			this.inside = $('<div>', { id: id, class: 'inside', contentEditable: 'true' }).html(self.module.definition.text || '');
+            if(this.module.getConfigurationCheckbox('editable', 'isEditable')) {
+    			this.inside = $('<div>', { id: id, class: 'inside', contentEditable: 'true' }).html(self.module.definition.text || '');
 			
-			require(['ckeditor'], function(CKEDITOR) {
-				if(done)
-					return;
-				CKEDITOR.disableAutoInline = true;
-				self.instance = CKEDITOR.inline(self._id, {
-					extraPlugins:"mathjax"
-				});
-				self.instance.on("change",function(){
-					self.module.definition.text = self.instance.getData();
-					self.module.getDomWrapper().height(self.inside.height() + 70);
-					Grid.moduleResize(self.module);
-				});
-				done = true;
-			});
+    			require(['ckeditor'], function(CKEDITOR) {
+    				if(done)
+    					return;
+    				CKEDITOR.disableAutoInline = true;
+    				self.instance = CKEDITOR.inline(self._id, {
+    					extraPlugins:"mathjax"
+    				});
+    				self.instance.on("change",function(){
+    					self.module.definition.text = self.instance.getData();
+    					self.module.getDomWrapper().height(self.inside.height() + 70);
+    					Grid.moduleResize(self.module);
+    				});
+    				done = true;
+    			});
+            }
+            
+            else {
+                this.inside = $('<div>', { id: id, class: 'inside'}).html(self.module.definition.text || '');
+            }
+            
+			this.dom = $('<div />', {  class: 'postit' }).css("font-family", this.module.getConfiguration("fontfamily")+", Arial");
+			
 
 			this.dom.html(this.inside);
 			this.module.getDomContent().html(this.dom);
-			
 			this.resolveReady();			
 		}
 		
