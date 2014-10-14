8ca9cacf41a9f056f08995b67c8e1972c037c0d3
Michaël Zasso <mic.besace@gmail.com>
2014-10-14 11:10:25 +0200

    rewrite 2dnmr module with jsGraph

--- src/modules/types/science/spectra/nmr/2dnmr/view.js
+++ src/modules/types/science/spectra/nmr/2dnmr/view.js
@@ -1,136 +1,298 @@
-define(['modules/default/defaultview', 'lib/plot/plot', 'src/util/datatraversing', 'components/jcampconverter/src/jcampconverter'], function(Default, Graph, Traversing, JcampConverter) {
-	
-	function view() {};
-	view.prototype = $.extend(true, {}, Default, {
-		
-		init: function() {
+'use strict';
 
-			var html = [];
-			html.push('<div class="2drmn"></div>');
-			this.namedSeries = {};
-			this.dom = $(html.join(''));
-			this.module.getDomContent().html(this.dom);
-			this.resolveReady();
-		},
+define(['modules/default/defaultview', 'components/jsgraph/dist/jsgraph.min', 'src/util/datatraversing', 'components/jcampconverter/src/jcampconverter'], function (Default, Graph, Traversing, JcampConverter) {
 
-		
-		inDom: function() {
-			var self = this;
-			var NMR = new Graph(this.dom.get(0), {});
-			this._instance = NMR;
+    function View() {
+    }
 
-			NMR.resetAxis();
-			NMR.resetSeries();
-			NMR.setOption('zoomMode', 'xy');
-			NMR.setOption('defaultWheelAction', 'toSeries');
-			NMR.setOption('defaultMouseAction', 'zoomXY');
-			NMR.setOption('shiftMouseAction', 'drag');
+    View.prototype = $.extend(true, {}, Default, {
 
-			NMR.getTopAxis(0, { flipped: true }).setLabel('Blahppm');
-			NMR.getLeftAxis(0, { flipped: false }).setLabel('ppm');
-		},
+        init: function () {
+            this.dom = $('<table cellpadding="0" cellspacing="0" class="nmr-wrapper"><tr><td></td><td class="nmr-1d nmr-1d-x nmr-main"></td></tr><tr class="nmr-main"><td class="nmr-1d nmr-1d-y"></td><td class="nmr-2d"></td></tr></table>');
+            this.module.getDomContent().html(this.dom);
+        },
 
-		onResize: function() {
-			this._instance.resize(this.width - 20, this.height - 20);
-		},
-		
-		update: {
-		
-			'jcampx': function(moduleValue) {
-				if(!this._instance)
-					return;
-			
-				this.addSerieJcampXOrY(moduleValue, true);
-				this.redraw();
+        inDom: function () {
 
-			},
+            var self = this;
 
-			'jcampy': function(moduleValue) {
-				if(!this._instance)
-					return;
-			
-				this.addSerieJcampXOrY(moduleValue, false);
-				this.redraw();
-			},
+            this.graphs = {};
+            this.series = {};
 
-			'jcampxy': function(moduleValue) {
+            // Create 2D graph
+            this.graphs['_2d'] = new Graph(this.dom.find('.nmr-2d').get(0), {
+                close: {
+                    left: false,
+                    top: false,
+                    right: false
+                },
+                paddingBottom: 0,
+                paddingTop: 0,
+                paddingLeft: 0,
+                paddingRight: 0,
+                plugins: {
+                    'graph.plugin.zoom': {
+                        zoomMode: 'xy',
+                        onZoomStart: function (graph, x, y, e) {
+                            self.graphs['x']._pluginExecute('graph.plugin.zoom', 'onMouseDown', [ self.graphs['x'], x, y, e, true ]);
+                            self.graphs['y']._pluginExecute('graph.plugin.zoom', 'onMouseDown', [ self.graphs['y'], x, y, e, true ]);
+                        },
+                        onZoomMove: function (graph, x, y, e) {
+                            self.graphs['x']._pluginExecute('graph.plugin.zoom', 'onMouseMove', [ self.graphs['x'], x, y, e, true ]);
+                            self.graphs['y']._pluginExecute('graph.plugin.zoom', 'onMouseMove', [ self.graphs['y'], x, y, e, true ]);
+                        },
+                        onZoomEnd: function (graph, x, y, e) {
+                            self.graphs['x']._pluginExecute('graph.plugin.zoom', 'onMouseUp', [ self.graphs['x'], x, y, e, true ]);
+                            self.graphs['y']._pluginExecute('graph.plugin.zoom', 'onMouseUp', [ self.graphs['y'], x, y, e, true ]);
+                        },
+                        onDblClick: function (x, y, prefs, e) {
+                            self.graphs['y']._pluginExecute('graph.plugin.zoom', 'onDblClick', [ self.graphs['y'], x, y, { mode: 'total' }, e, true ]);
+                            self.graphs['x']._pluginExecute('graph.plugin.zoom', 'onDblClick', [ self.graphs['x'], x, y, { mode: 'total' }, e, true ]);
+                        }
+                    }
+                },
+                dblclick: {
+                    type: 'plugin',
+                    plugin: 'graph.plugin.zoom',
+                    options: {
+                        mode: 'total'
+                    }
+                },
+                pluginAction: {
+                    'graph.plugin.zoom': { shift: false, ctrl: false },
+                    'graph.plugin.shape': { shift: true, ctrl: false }
+                },
+                wheel: {
+                    type: 'toSeries'
+                }
+            });
 
-				if(!this._instance)
-					return;
-			
-				value = JcampConverter(Traversing.getValueIfNeeded(moduleValue), {lowres: 1024});
-				this.addSerieXOrY(value, true);
-				this.addSerieXOrY(value, false);
+            // Create 2D serie
+            this.series['_2d'] = this.graphs['_2d'].newSerie('serie2d', {}, 'contour')
+                .autoAxis();
+            this.series['_2d'].getXAxis()
+                .setLabel('ppm')
+                .togglePrimaryGrid(false)
+                .toggleSecondaryGrid(false)
+                .setDisplay(false)
+                .flip(true);
+            this.series['_2d'].getYAxis()
+                .togglePrimaryGrid(false)
+                .toggleSecondaryGrid(false)
+                .setDisplay(false)
+                .flip(true);
 
-				this.redraw();
-			},
+            // Create 1D x graph
+            this.graphs['x'] = new Graph(this.dom.find('.nmr-1d-x').get(0), {
+                close: {
+                    left: false,
+                    top: false,
+                    right: false
+                },
+                paddingBottom: 0,
+                paddingTop: 0,
+                paddingLeft: 0,
+                paddingRight: 0,
+                plugins: {
+                    'graph.plugin.zoom': {
+                        zoomMode: 'x',
+                        onZoomStart: function (graph, x, y, e) {
+                            self.graphs[ '_2d' ]._pluginExecute('graph.plugin.zoom', 'onMouseDown', [ self.graphs[ '_2d' ], x, undefined, e, true ]);
+                        },
+                        onZoomMove: function (graph, x, y, e) {
+                            self.graphs[ '_2d' ]._pluginExecute('graph.plugin.zoom', 'onMouseMove', [ self.graphs[ '_2d' ], x, undefined, e, true ]);
+                        },
+                        onZoomEnd: function (graph, x, y, e) {
+                            self.graphs[ '_2d' ]._pluginExecute('graph.plugin.zoom', 'onMouseUp', [ self.graphs[ '_2d' ], x, undefined, e, true ]);
+                        },
+                        onDblClick: function (x, y, prefs, e) {
+                            self.graphs[ '_2d' ]._pluginExecute('graph.plugin.zoom', 'onDblClick', [ self.graphs[ '_2d' ], x, y, { mode: 'xtotal' }, e, true ]);
+                        }
+                    }
+                },
+                dblclick: {
+                    type: 'plugin',
+                    plugin: 'graph.plugin.zoom',
+                    options: {
+                        mode: 'total'
+                    }
+                },
+                pluginAction: {
+                    'graph.plugin.zoom': { shift: false, ctrl: false },
+                    'graph.plugin.shape': { shift: true, ctrl: false }
+                }
+            });
 
-			'jcamp2d': function(moduleValue) {
+            // Created 1D y serie
+            this.series['x'] = this.graphs['x'].newSerie('serieX', { useSlots: true })
+                .autoAxis();
+            this.series['x'].getYAxis()
+                .setDisplay(false)
+                .togglePrimaryGrid(false)
+                .toggleSecondaryGrid(false);
+            this.series['x'].getXAxis()
+                .flip(true)
+                .setLabel('ppm')
+                .togglePrimaryGrid(false)
+                .toggleSecondaryGrid(false).
+                setTickPosition('outside');
 
-				if(!this._instance)
-					return;
-				var value = JcampConverter(Traversing.getValueIfNeeded(moduleValue));
-				if(!value.contourLines)
-					return;
-				var NMR = this._instance;
-					NMR.resetSeries();
-				var serie = NMR.newSerie('2d' + Date.now(), {}, 'contour');
-				serie.setData(value.contourLines);
-				serie.setXAxis(NMR.getTopAxis());
-				serie.setYAxis(NMR.getLeftAxis());
-				this.redraw();
-			},
+            // Create 1D y graph
+            this.graphs['y'] = new Graph(this.dom.find('.nmr-1d-y').get(0), {
+                close: {
+                    left: false,
+                    top: false,
+                    right: false
+                },
+                paddingBottom: 0,
+                paddingTop: 0,
+                paddingLeft: 0,
+                paddingRight: 10,
+                plugins: {
+                    'graph.plugin.zoom': {
+                        zoomMode: 'y',
+                        onZoomStart: function (graph, x, y, e) {
+                            self.graphs[ '_2d' ]._pluginExecute('graph.plugin.zoom', 'onMouseDown', [ self.graphs[ '_2d' ], undefined , y, e, true ]);
+                        },
+                        onZoomMove: function (graph, x, y, e) {
+                            self.graphs[ '_2d' ]._pluginExecute('graph.plugin.zoom', 'onMouseMove', [ self.graphs[ '_2d' ], undefined , y, e, true ]);
+                        },
+                        onZoomEnd: function (graph, x, y, e) {
+                            self.graphs[ '_2d' ]._pluginExecute('graph.plugin.zoom', 'onMouseUp', [ self.graphs[ '_2d' ], undefined, y, e, true ]);
+                        },
+                        onDblClick: function (x, y, prefs, e) {
+                            self.graphs[ '_2d' ]._pluginExecute('graph.plugin.zoom', 'onDblClick', [ self.graphs[ '_2d' ], x, y, { mode: 'ytotal' }, e, true ]);
+                        }
+                    }
+                },
+                dblclick: {
+                    type: 'plugin',
+                    plugin: 'graph.plugin.zoom',
+                    options: {
+                        mode: 'total'
+                    }
+                },
+                pluginAction: {
+                    'graph.plugin.zoom': { shift: false, ctrl: false },
+                    'graph.plugin.shape': { shift: true, ctrl: false }
+                },
+                wheel: {
+                    type: 'plugin',
+                    plugin: 'graph.plugin.zoom',
+                    options: {
+                        direction: 'x'
+                    }
+                }
+            });
 
-			annotations: function(value) {
-				value = DataTraversing.getValueIfNeeded(value);
+            // Create 1D y serie
+            this.series['y'] = this.graphs['y'].newSerie('serieY', { flip: true, useSlots: true })
+                .setXAxis(self.graphs['y'].getBottomAxis())
+                .setYAxis(self.graphs['y'].getRightAxis());
+            this.series['y'].getYAxis()
+                .setLabel('ppm')
+                .togglePrimaryGrid(false)
+                .toggleSecondaryGrid(false)
+                .flip(true)
+                .setTickPosition('outside');
+            this.series['y'].getXAxis()
+                .togglePrimaryGrid(false)
+                .toggleSecondaryGrid(false)
+                .setDisplay(false)
+                .flip(true);
 
-				console.log(value);
-				if(!value)
-					return;
+            this.resolveReady();
 
-				this.annotations = value;
-				this.resetAnnotations(true);
-			}
-		},
+        },
+
+        onResize: function () {
+            if (this.graphs['_2d']) {
+                this.graphs['_2d'].resize(this.width - 60, this.height - 60);
+            }
+            if (this.graphs['x']) {
+                this.graphs['x'].resize(this.width - 60, 50);
+            }
+            if (this.graphs['y']) {
+                this.graphs['y'].resize(50, this.height - 60);
+            }
+            this.redraw();
+        },
+
+        update: {
+
+            jcampx: function (moduleValue) {
+                this.addSerieJcampXOrY(moduleValue, true, false);
+            },
+
+            jcampy: function (moduleValue) {
+                this.addSerieJcampXOrY(moduleValue, false, true);
+            },
+
+            jcampxy: function (moduleValue) {
+                this.addSerieJcampXOrY(moduleValue, true, true);
+            },
+
+            jcamp2d: function (moduleValue) {
+                var self = this;
+                JcampConverter(String(moduleValue.get())).then(function (result) {
+                    var data = result.contourLines;
+                    self.series['_2d'].setData(data);
+                    self.redraw();
+                });
+            },
+
+            annotations: function (value) {
+                /*
+                 TODO annotations ?
+                 */
+                /* value = DataTraversing.getValueIfNeeded(value);
+
+                 console.log(value);
+                 if (!value)
+                 return;
+
+                 this.annotations = value;
+                 this.resetAnnotations(true);*/
+            }
+        },
 
 
-		addSerieJcampXOrY: function(value, x) {
-			value = Traversing.getValueIfNeeded(value);
-			spectra = JcampConverter(value, {lowres: 1024});
-			this.addSerieXOrY(spectra, x);
-		},
+        addSerieJcampXOrY: function (value, x, y) {
+            var self = this;
+            JcampConverter(String(value.get())).then(function (result) {
+                var data = result.spectra[0].data[0];
+                if (x) {
+                    self.series['x'].setData(data);
+                }
+                if (y) {
+                    self.series['y'].setData(data);
+                }
+                self.redraw();
+            });
+        },
 
-		addSerieXOrY: function(spectra, x) {
+        redraw: function () {
+            if (this.graphs['y']) {
+                this.graphs['y'].redraw();
+                this.graphs['y'].autoscaleAxes();
+                this.graphs['y'].drawSeries();
+            }
+            if (this.graphs['x']) {
+                this.graphs['x'].redraw();
+                this.graphs['x'].autoscaleAxes();
+                this.graphs['x'].drawSeries();
+            }
+            if (this.graphs['_2d']) {
+                this.graphs['_2d'].redraw();
+                this.graphs['_2d'].autoscaleAxes();
+                this.graphs['_2d'].drawSeries();
+            }
+        }
 
-			var NMR = this._instance,
-				axis = x ? NMR.getTopAxis() : NMR.getLeftAxis();
+    });
 
-			axis.killSeries(true);
-			spectra = spectra.spectra;
-			for (var i = 0, l = spectra.length; i<l; i++) {
+    return View;
 
-				serie = axis.addSerie((x ? 'x' : 'y') + Date.now(), {
-					trackMouse: false, 
-					trackMouseLabel: '<x>', 
-					trackMouseLabelRouding: 3,
-					zoomMode: x ? 'x' : 'y'
-				});
-				serie.setData(spectra[i].data[spectra[i].data.length - 1]);
-				if(!x)
-					serie.setFlip(true);
-				break;
-			}
-		},
-
-		redraw: function() {
-			this._instance.redraw();
-			this._instance.drawSeries();
-		}
-
-
-	});
-
-	return view;
 });
 
  
