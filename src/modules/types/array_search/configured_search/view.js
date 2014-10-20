74fba12f7c71fc0a1e3d8f9214f13357b14d9431
Michaël Zasso <mic.besace@gmail.com>
2014-10-20 09:40:09 +0200

    array_search: fix function builder

--- src/modules/types/array_search/configured_search/view.js
+++ src/modules/types/array_search/configured_search/view.js
@@ -257,7 +257,7 @@ define(['modules/default/defaultview', 'src/util/datatraversing', 'src/util/api'
                         if (searchfields[ i ].groups.general[ 0 ].type[ 0 ] === 'float') opts.number = true;
                         if (searchfields[ i ].groups.text && searchfields[ i ].groups.text[ 0 ].case_sensitive[ 0 ][ 0 ] === 'case_sensitive') opts.caseSensitive = true;
                         var allow_undefined = !!(searchfields[ i ].groups.general[ 0 ].allow_undefined ? searchfields[ i ].groups.general[ 0 ].allow_undefined[0].length : 0);
-                        toEval += ' ( ( el = self.getJpath( "' + searchOn[ j ] + ', row ) ) ? ( ';
+                        toEval += ' ( ( el = self.getJpath( "' + searchOn[ j ] + '", row ) ) ? ( ';
                         toEval += this._makeOp(searchfields[ i ].groups.general[ 0 ].operator[ 0 ], searchfields[ i ].groups.general[ 0 ].name[ 0 ], opts);
                         toEval += ' ) : ' + allow_undefined + ' ) ';
 
