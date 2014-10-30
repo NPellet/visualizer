3b61d7483ae324d344a4c69aa68ad97c6f41d8e2
Michaël Zasso <mic.besace@gmail.com>
2014-10-30 15:49:03 +0100

    header clean code and use promises

--- src/src/header/header.js
+++ src/src/header/header.js
@@ -1,96 +1,90 @@
 'use strict';
 
-define(['require', 'jquery', 'src/util/versioning'], function(require, $, Versioning) {
+define(['require', 'jquery', 'src/util/versioning'], function (require, $, Versioning) {
 
-	var elements = [];
-	return {
+    var elements = [];
+    return {
 
-		init: function(headerConfig) {
-			var self = this;
+        init: function (headerConfig) {
+            var self = this;
 
-			if (headerConfig.elements) {
-				this.loadHeaderElements(headerConfig.elements);
-			}
+            if (headerConfig.elements) {
+                this.loadHeaderElements(headerConfig.elements);
+            }
 
-			this.dom = $('<div id="header"><div id="title"><div></div></div></div>');
-			$("#ci-visualizer").prepend(this.dom);
+            this.dom = $('<div id="header"><div id="title"><div></div></div></div>');
+            $('#ci-visualizer').prepend(this.dom);
 
-			this.setHeight(headerConfig.height || "30px");
+            this.setHeight(headerConfig.height || '30px');
 
-			this._titleDiv = $("#title").children('div');
-			this._titleDiv.attr('contenteditable', 'true').bind('keypress', function(e) {
-				e.stopPropagation();
-				if (e.keyCode !== 13)
-					return;
-				e.preventDefault();
-				$(this).trigger('blur');
-			})
-				.bind('blur', function() {
-					Versioning.getView().configuration.set('title', $(this).text().replace(/[\r\n]/g, ""));
-				});
+            this._titleDiv = $('#title').children('div');
+            this._titleDiv.attr('contenteditable', 'true').bind('keypress', function (e) {
+                e.stopPropagation();
+                if (e.keyCode !== 13)
+                    return;
+                e.preventDefault();
+                $(this).trigger('blur');
+            })
+                .bind('blur', function () {
+                    Versioning.getView().configuration.set('title', $(this).text().replace(/[\r\n]/g, ''));
+                });
 
-
-			Versioning.getViewHandler( ).versionChange( ).progress(function(el) {
-
-				self.setTitle(el);
-
-			});
-
-		},
-
-		setHeight: function(height) {
-			this.dom.css('height', height);
-			$("#modules-grid").css('margin-top', height);
-		},
-
-		setTitle: function(view) {
-			this._titleDiv.text(view.configuration ? view.configuration.title : 'Untitled');
-		},
-		
-		loadHeaderElements: function(all) {
-			if(!$.isArray(all))
-				return;
-
-			var self = this,
-				i = 0, 
-				l = all.length;
-
-			for (; i < l; i++) {
-				this.addHeaderElement(i, this.createElement(all[i]));
-			}
-
-			$.when.apply($.when, elements).then(function() {
-				self.buildHeaderElements(arguments);
-			});
-		},
-
-		addHeaderElement: function(i, el) {
-			elements[i] = el;
-		},
-
-		createElement: function(source) {
-			var def = $.Deferred();
-			
-            require(['./components/'+source.type], function(El) {
-                var el = new El();
-                el.init(source);
-                def.resolve(el);
+            Versioning.getViewHandler().versionChange().progress(function (el) {
+                self.setTitle(el);
             });
 
-			return def.promise();
-		},
+        },
 
-		buildHeaderElements: function(elements) {
-			if(this.ul)
-				this.ul.empty();
+        setHeight: function (height) {
+            this.dom.css('height', height);
+            $('#modules-grid').css('margin-top', height);
+        },
 
-			this.ul = this.ul || $("<ul />").appendTo(this.dom);
-			var i = 0, l = elements.length;
-			for( ; i < l; i++) {
-				this.ul.append(elements[i].getDom());
-			}
-		}
+        setTitle: function (view) {
+            this._titleDiv.text(view.configuration ? view.configuration.title : 'Untitled');
+        },
 
-	};
+        loadHeaderElements: function (all) {
+            if (!$.isArray(all))
+                return;
+
+            var self = this,
+                i = 0,
+                l = all.length;
+
+            for (; i < l; i++) {
+                this.addHeaderElement(i, this.createElement(all[i]));
+            }
+
+            Promise.all(elements).then(self.buildHeaderElements.bind(self));
+
+        },
+
+        addHeaderElement: function (i, el) {
+            elements[i] = el;
+        },
+
+        createElement: function (source) {
+            return new Promise(function (resolve, reject) {
+                require(['./components/' + source.type], function (El) {
+                    var el = new El();
+                    el.init(source);
+                    resolve(el);
+                });
+            });
+        },
+
+        buildHeaderElements: function (elements) {
+            if (this.ul)
+                this.ul.empty();
+
+            this.ul = this.ul || $('<ul />').appendTo(this.dom);
+            var i = 0, l = elements.length;
+            for (; i < l; i++) {
+                this.ul.append(elements[i].getDom());
+            }
+        }
+
+    };
 
 });
