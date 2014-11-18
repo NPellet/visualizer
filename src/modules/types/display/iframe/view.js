2724ab2d02b4cdd2c3a80f272d9b42f50cde2a99
Daniel Kostro <kostro.d@gmail.com>
2014-11-18 09:43:35 +0100

    iframe: remove module's scrollbar (only iframe scrollbar left)

--- src/modules/types/display/iframe/view.js
+++ src/modules/types/display/iframe/view.js
@@ -3,8 +3,13 @@ define(['modules/default/defaultview'], function(Default) {
 	function view() {};
 	view.prototype = $.extend(true, {}, Default, {
 		
-		init: function() {	
+		init: function() {
 			this.dom = $('<iframe border="0" frameborder="none" width="100%" height="100%" />');
+
+			// fix scroll bar
+			// see http://stackoverflow.com/a/12726445/1247233 for explanations
+			this.dom.css('vertical-align', 'bottom');
+
 			this.module.getDomContent().html(this.dom);
 			this.resolveReady();
 		},
