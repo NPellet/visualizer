7f2a96cfb3d4a1f239c5569795adfcec2f5bad84
Michaël Zasso <mic.besace@gmail.com>
2014-11-19 14:35:58 +0100

    add config file for doc

--- gruntfile.js
+++ gruntfile.js
@@ -291,7 +291,7 @@ module.exports = function (grunt) {
                 options: {
                     destination: 'doc',
                     template : 'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template',
-                    configure : 'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template/jsdoc.conf.json'
+                    configure : 'doc.conf.json'
                 }
             }
         }
