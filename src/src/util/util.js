200c0b62911a185d327106f932191655da0e6882
Michaël Zasso <mic.besace@gmail.com>
2014-12-10 11:20:44 +0100

    util : add deprecate method

--- src/src/util/util.js
+++ src/src/util/util.js
@@ -4,15 +4,15 @@
  * Global utility methods
  * @module src/util/util
  */
-define(['src/util/debug'], function (Debug) {
+define(['src/util/debug', 'src/util/color'], function (Debug, Color) {
 
     var uniqueid = 0;
 
     var months = ['January', 'February', 'March', 'April', 'Mai', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-    var noop = function () {
-    };
+    function noop() {
+    }
 
     var regQuote = /"/g,
         regJpath = /^element\./;
@@ -171,84 +171,8 @@ define(['src/util/debug'], function (Debug) {
 
 
         },
-        getDistinctColors: function (numColors) {
 
-            var colors = [], j = 0;
-            for (var i = 0; i < 360; i += 360 / numColors) {
-                j++;
-                var color = this.hsl2rgb(i, 100, 30 + j % 4 * 15);
-                colors.push([Math.round(color.r * 255), Math.round(color.g * 255), Math.round(color.b * 255)]);
-            }
-            return colors;
-        },
-        getNextColorRGB: function (colorNumber, numColors) {
-            return this.getDistinctColors(numColors)[colorNumber];
-        },
-        hsl2rgb: function (h, s, l) {
-            var m1, m2, hue, r, g, b;
-            s /= 100;
-            l /= 100;
-
-            if (s === 0)
-                r = g = b = (l * 255);
-            else {
-                if (l <= 0.5)
-                    m2 = l * (s + 1);
-                else
-                    m2 = l + s - l * s;
-
-                m1 = l * 2 - m2;
-                hue = h / 360;
-                r = this.hueToRgb(m1, m2, hue + 1 / 3);
-                g = this.hueToRgb(m1, m2, hue);
-                b = this.hueToRgb(m1, m2, hue - 1 / 3);
-            }
-            return {r: r, g: g, b: b};
-        },
-        hueToRgb: function (p, q, t) {
-            if (t < 0)
-                t += 1;
-            if (t > 1)
-                t -= 1;
-            if (t < 1 / 6)
-                return p + (q - p) * 6 * t;
-            if (t < 1 / 2)
-                return q;
-            if (t < 2 / 3)
-                return p + (q - p) * (2 / 3 - t) * 6;
-            return p;
-        },
-        hexToRgb: function (hex) {
-            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-            return result ? [
-                parseInt(result[1], 16),
-                parseInt(result[2], 16),
-                parseInt(result[3], 16)
-
-            ] : [0, 0, 0];
-        },
-        rgbToHex: function (r, g, b) {
-            return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-        },
-        getColor: function (color) {
-
-            if (Array.isArray(color)) {
-                switch (color.length) {
-                    case 3:
-                        return 'rgb(' + color.join(',') + ')';
-                        break;
-                    case 4:
-                        return 'rgba(' + color.join(',') + ')';
-                        break;
-                }
-            } else if (typeof(color) == 'object') {
-                return 'rgb(' + Math.round(color.r * 255) + ', ' + Math.round(color.g * 255) + ', ' + Math.round(color.b * 255) + ')';
-            }
-
-            return color;
-        },
-
-        getCssVendorPrefix: function() {
+        getCssVendorPrefix: function () {
             var styles = window.getComputedStyle(document.documentElement, '');
             var pre = (Array.prototype.slice
                 .call(styles)
@@ -357,15 +281,23 @@ define(['src/util/debug'], function (Debug) {
             }
 
             return access;
-        }
+        },
+
+        // Deprecated color methods. Moved to src/util/color
+        getDistinctColors: deprecate(Color.getDistinctColors, 'use Color.getDistinctColors'),
+        getNextColorRGB: deprecate(Color.getNextColorRGB, 'use Color.getNextColorRGB'),
+        hsl2rgb: deprecate(Color.hsl2rgb, 'use Color.hsl2rgb'),
+        hueToRgb: deprecate(Color.hue2rgb, 'use Color.hue2rgb'),
+        hexToRgb: deprecate(Color.hex2rgb, 'use Color.hex2rgb'),
+        rgbToHex: deprecate(Color.rgb2hex, 'use Color.rgb2hex'),
+        getColor: deprecate(Color.getColor, 'use Color.getColor')
 
     };
 
     /**
      * No-op function
      */
-    exports.noop = function () {
-    };
+    exports.noop = noop;
 
     function isInt(str) {
         return isNaN(str) ? NaN : parseInt(str);
@@ -481,6 +413,26 @@ define(['src/util/debug'], function (Debug) {
             return 0;
         }
     };
+
+    /**
+     * Mark that a method should not be used. Returns a modified function which warns once when called.
+     * @param {Function} method - the deprecated method
+     * @param {string} [message] - optional message to log
+     */
+    exports.deprecate = deprecate;
+    function deprecate(method, message) {
+        var warned = false;
+        return function deprecated() {
+            if (!warned) {
+                if (Debug.getDebugLevel() >= Debug.Levels.WARN) {
+                    Debug.warn('Method ' + method.name + ' is deprecated. ' + (message || ''));
+                    console.trace();
+                }
+                warned = true;
+            }
+            return method.apply(this, arguments);
+        }
+    }
 
     return exports;
 
