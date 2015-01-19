6bf3ace0e902b6cc6123fa423a2ddb38a940fb0c
Daniel Kostro <kostro.d@gmail.com>
2015-01-19 12:18:17 +0100

    slick_grid: change editor async delay

--- src/modules/types/edition/slick_grid/view.js
+++ src/modules/types/edition/slick_grid/view.js
@@ -214,6 +214,7 @@ define(['require', 'modules/default/defaultview', 'src/util/debug', 'lodash', 's
                 forceFitColumns: that.module.getConfigurationCheckbox('slickCheck', 'forceFitColumns'),
                 multiColumnSort: that.module.getConfigurationCheckbox('slickCheck', 'multiColumnSort'),
                 asyncEditorLoading: true,
+                asyncEditorLoadDelay: 40,
                 enableAsyncPostRender: true,
                 asyncPostRenderDelay: 0,
                 defaultColumnWidth: that.module.getConfiguration('slick.defaultColumnWidth') || 80,
