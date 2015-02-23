d78e0694820b98bd733a941b51c1ef3928962067
Michaël Zasso <mic.besace@gmail.com>
2015-02-23 11:39:21 +0100

    add external worker loader

---
+++ src/src/util/worker.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var blobCache = {};
+
+define(['components/superagent/superagent'], function (agent) {
+
+    return function getWorker(url) {
+        return new Promise(function (resolve, reject) {
+            if (blobCache[url]) {
+                return resolve(new Worker(blobCache[url]));
+            }
+            agent.get(url).end(function (err, res) {
+                if (err) {
+                    return reject(err);
+                }
+                if (res.status !== 200) {
+                    return reject(new Error('Failed to download worker at ' + url))
+                }
+                var blob = new Blob([res.text], {type: 'application/javascript'});
+                var blobUrl = URL.createObjectURL(blob);
+                blobCache[url] = blobUrl;
+                resolve(new Worker(blobUrl));
+            });
+        });
+    };
+
+});
