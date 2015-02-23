53a772b40ab00e425527d7b34804399e098b45aa
Michaël Zasso <mic.besace@gmail.com>
2015-02-23 12:12:34 +0100

    module: rich_text: fix html update in read-only mode
    fixes #365

--- src/modules/types/edition/rich_text/view.js
+++ src/modules/types/edition/rich_text/view.js
@@ -1,3 +1,5 @@
+'use strict';
+
 define([
     'modules/default/defaultview',
     'src/util/util',
@@ -6,98 +8,95 @@ define([
     'src/util/typerenderer',
     'src/main/grid',
     'chroma'
-    ], function(Default, Util, CKEDITOR, _, Renderer, Grid, chroma) {
+], function (Default, Util, CKEDITOR, _, Renderer, Grid, chroma) {
 
     function View() {
         this._id = Util.getNextUniqueId();
     }
 
     View.prototype = $.extend(true, {}, Default, {
-        init: function() {
-
-        },
-        inDom: function() {
+        inDom: function () {
             this.initEditor();
             this.resolveReady();
         },
-
-        update: {
-            html: function(moduleValue) {
-                this.module.data = moduleValue;
-
-                this.module.definition.richtext = moduleValue.get();
-                //this.initEditor();
-                this.updateEditor(moduleValue.get());
+        blank: {
+            html: function () {
+                if (this.isEditable) {
+                    this.updateEditor('');
+                } else {
+                    this.updateHtml('');
+                }
             }
         },
-
-        initEditor: function() {
-            var self = this;
-
-
-            var initText = this.module.definition.richtext || '';
-            if(!this.module.getConfigurationCheckbox('editable', 'isEditable')) {
-                var def = Renderer.toScreen({
-                    type: 'html',
-                    value: initText
-                }, this.module );
-                def.always( function() {
-                    self.dom = $( '<div id="'+this._id+ '">');
-                    self.dom.html(initText);
-                    self.module.getDomContent().html(self.dom);
-                    self._setCss();
-                });
+        update: {
+            html: function (moduleValue) {
+                this.module.data = moduleValue;
+                var val = moduleValue.get();
+                this.module.definition.richtext = val;
+                if (this.isEditable) {
+                    this.updateEditor(val);
+                } else {
+                    this.updateHtml(val);
+                }
             }
-            else {
-                this.dom = $(' <div id="'+this._id+'" contenteditable="true">');
+        },
+        initEditor: function () {
+            var self = this;
+            var initText = this.module.definition.richtext || '';
+            this.isEditable = this.module.getConfigurationCheckbox('editable', 'isEditable');
+            if (!this.isEditable) {
+                this.dom = $('<div id="' + self._id + '">');
+                this.module.getDomContent().html(self.dom);
+                this._setCss();
+                this.updateHtml(initText);
+            } else {
+                this.dom = $(' <div id="' + this._id + '" contenteditable="true">');
                 this.dom.html(initText);
                 this.module.getDomContent().html(this.dom);
                 this._setCss();
                 this.module.controller.valueChanged(initText);
             }
-
-            if(CKEDITOR.instances[this._id]) {
+            if (CKEDITOR.instances[this._id]) {
                 CKEDITOR.instances[this._id].destroy();
             }
-            if(this.module.getConfigurationCheckbox('editable', 'isEditable')) {
+            if (this.module.getConfigurationCheckbox('editable', 'isEditable')) {
                 CKEDITOR.disableAutoInline = true;
                 this.instance = CKEDITOR.inline(this._id, {
-                    extraPlugins:"mathjax,font,sourcedialog,codesnippet",
-                    removeButtons:''
+                    extraPlugins: 'mathjax,font,sourcedialog,codesnippet',
+                    removeButtons: ''
                 });
-                this.instance.on("change",function(){
+                this.instance.on('change', function () {
                     self.module.controller.valueChanged(self.instance.getData());
-                    if(self.module.getConfigurationCheckbox('autoHeight', 'yes')) {
-                        self.module.getDomWrapper().height(self.getContentHeight() +50);
+                    if (self.module.getConfigurationCheckbox('autoHeight', 'yes')) {
+                        self.module.getDomWrapper().height(self.getContentHeight() + 50);
                         Grid.moduleResize(self.module);
                     }
 
                 });
             }
         },
-
-        updateEditor: function(html) {
+        updateEditor: function (html) {
             this.instance.setData(html);
         },
-
-        getContentHeight: function() {
+        updateHtml: function (html) {
+            this.dom.html(html);
+        },
+        getContentHeight: function () {
             var height = 0;
-            this.dom.children().each(function() {
+            this.dom.children().each(function () {
                 height += $(this).height();
             });
             return height;
         },
-        
-        _setCss: function() {
+        _setCss: function () {
             var bgColor = this.module.getConfiguration('bgColor');
             this.dom.css({
                 height: '100%',
                 width: '100%',
-                padding: "5px",
-                boxSizing: "border-box"
+                padding: '5px',
+                boxSizing: 'border-box'
             });
-
-            if(this.module.getConfigurationCheckbox('postit', 'yes')) {
+            if (this.module.getConfigurationCheckbox('postit', 'yes')) {
                 var ch = chroma(bgColor[0], bgColor[1], bgColor[2]);
                 this.dom.addClass('richtext-postit');
                 this.dom.parents('.ci-module-wrapper').addClass('ci-module-richtext-postit');
@@ -120,4 +119,5 @@ define([
     });
 
     return View;
+
 });
