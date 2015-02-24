47cd79f24b1af7fdd80b52f488b6fc0d7f8e58ab
Daniel Kostro <kostro.d@gmail.com>
2015-02-24 08:10:44 +0100

    Clean markdown filter

--- src/usr/filters/markdown.js
+++ src/usr/filters/markdown.js
@@ -1,20 +1,15 @@
 'use strict';
 
-define(['lodash', 'src/util/util', 'marked', 'highlightjs', 'components/markdown-js/lib/markdown'], function (_, Util, marked, highlights) {
+define(['lodash', 'src/util/util', 'marked', 'highlightjs'], function (_, Util, marked, highlights) {
     var cssPromises = [];
-    cssPromises.push(Util.loadCss('./components/highlight.js/src/styles/github.css'));
+    cssPromises.push(Util.loadCss('./components/highlight.js/src/styles/default.css'));
     var cssLoaded = Promise.all(cssPromises);
-
 
     return {
         filter: function gcFilter(md, resolve) {
-            // init. We check ourselves what languages where registered
-            // since highlight.js systematically re-registers already
-            // registered languages
             cssLoaded.then(function() {
                 resolve({
                     type: 'html',
-                    //value: markdown.toHTML(md.resurrect())
                     value: marked(md.resurrect(), {
                         highlight: function (code) {
                             return highlights.highlightAuto(code).value;
