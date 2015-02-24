4e022be231fbca3561127fd89635b178459ed16c
Daniel Kostro <kostro.d@gmail.com>
2015-02-24 08:03:43 +0100

    Add marked and highlight libraries

--- src/usr/filters/markdown.js
+++ src/usr/filters/markdown.js
@@ -1,13 +1,28 @@
 'use strict';
 
-define(['components/markdown-js/lib/markdown'], function () {
+define(['lodash', 'src/util/util', 'marked', 'highlightjs', 'components/markdown-js/lib/markdown'], function (_, Util, marked, highlights) {
+    var cssPromises = [];
+    cssPromises.push(Util.loadCss('./components/highlight.js/src/styles/github.css'));
+    var cssLoaded = Promise.all(cssPromises);
+
 
     return {
         filter: function gcFilter(md, resolve) {
-            resolve({
-                type: 'html',
-                value: markdown.toHTML(md.resurrect())
+            // init. We check ourselves what languages where registered
+            // since highlight.js systematically re-registers already
+            // registered languages
+            cssLoaded.then(function() {
+                resolve({
+                    type: 'html',
+                    //value: markdown.toHTML(md.resurrect())
+                    value: marked(md.resurrect(), {
+                        highlight: function (code) {
+                            return highlights.highlightAuto(code).value;
+                        }
+                    })
+                });
             });
+
         }
     };
 });
