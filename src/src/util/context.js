cd382be455328c49e0c19341af7f29e69e231ad8
Daniel Kostro <kostro.d@gmail.com>
2015-02-25 16:43:31 +0100

    Clean context.js

--- src/src/util/context.js
+++ src/src/util/context.js
@@ -1,148 +1,146 @@
+'use strict';
+define(['jquery', 'src/util/api', 'modules/modulefactory', 'jquery-ui/menu'], function ($, API, ModuleFactory) {
 
-// Context menu
-define(['jquery', 'src/util/api', 'modules/modulefactory', 'jquery-ui/menu'], function($, API, ModuleFactory) {
-
-	var contextMenu;
+    var contextMenu;
 
     return {
 
-        listen: function(dom, elements, onBeforeShow, onAfterShow) {
-            if(!Array.isArray(elements[0]))
+        listen: function (dom, elements, onBeforeShow, onAfterShow) {
+            if (!Array.isArray(elements[0]))
                 elements = [elements];
 
-            dom.addEventListener('contextmenu', function(e) {
+            dom.addEventListener('contextmenu', function (e) {
 
 
-                if( onBeforeShow ) {
-                    onBeforeShow( contextMenu );
+                if (onBeforeShow) {
+                    onBeforeShow(contextMenu);
                 }
 
-                for(var i = 0, l = elements.length; i < l; i++) {
-                    (function(element, callbackClick, callbackOpen) {
-                        if(API.isViewLocked() ||  (API.getContextMenu().indexOf('all') === -1 && API.getContextMenu().indexOf(element.attr('name') || 'undefined') === -1)) return;
-                        if( ( callbackOpen && callbackOpen( e, element ) ) || ! callbackOpen ) {
-                            contextMenu.append( element );
+                for (var i = 0, l = elements.length; i < l; i++) {
+                    (function (element, callbackClick, callbackOpen) {
+                        if (API.isViewLocked() || (API.getContextMenu().indexOf('all') === -1 && API.getContextMenu().indexOf(element.attr('name') || 'undefined') === -1)) return;
+                        if (( callbackOpen && callbackOpen(e, element) ) || !callbackOpen) {
+                            contextMenu.append(element);
                         }
 
-                        element.bind('click', function( e2 ) {
+                        element.bind('click', function (e2) {
 
-                            if( callbackClick ) {
+                            if (callbackClick) {
 
-                                callbackClick.call( this, e, e2 );
+                                callbackClick.call(this, e, e2);
                             }
                         })
 
-                    }
-                        ) ( $( elements[ i ][ 0 ] ), elements[ i ][ 1 ], elements[ i ][ 2 ] );
+                    })($(elements[i][0]), elements[i][1], elements[i][2]);
                 }
 
-                if( onAfterShow ) {
-                    onAfterShow( contextMenu );
+                if (onAfterShow) {
+                    onAfterShow(contextMenu);
                 }
 
             }, true);
         },
 
-		unlisten: function(dom) {
-			dom.removeEventListener('contextmenu');
-		},
+        unlisten: function (dom) {
+            dom.removeEventListener('contextmenu');
+        },
 
-		getRootDom: function() {
-			return this.dom;
-		},
-		
-		init: function(dom) {
-			this.dom = dom;
-			var top, left;
-			dom.addEventListener('contextmenu', function(e) {
+        getRootDom: function () {
+            return this.dom;
+        },
 
-				//e.preventDefault();
-				if( contextMenu ) {
-					if( contextMenu.hasClass('ui-menu') ) {
-						contextMenu.menu('destroy')
-					}
-					contextMenu.remove();
-				} 
+        init: function (dom) {
+            this.dom = dom;
+            var top, left;
+            dom.addEventListener('contextmenu', function (e) {
 
-				contextMenu = null;
-				top = e.clientY;
-				left = e.clientX;
-				var $menu = $('<ul class="ci-contextmenu"></ul>').css({
-					'position': 'fixed',
-					'left': left,
-					'top': top,
-					'z-index': 10000
-				}).appendTo($("body"));
+                //e.preventDefault();
+                if (contextMenu) {
+                    if (contextMenu.hasClass('ui-menu')) {
+                        contextMenu.menu('destroy')
+                    }
+                    contextMenu.remove();
+                }
 
-				contextMenu = $menu;
+                contextMenu = null;
+                top = e.clientY;
+                left = e.clientX;
+                var $menu = $('<ul class="ci-contextmenu"></ul>').css({
+                    'position': 'fixed',
+                    'left': left,
+                    'top': top,
+                    'z-index': 10000
+                }).appendTo($("body"));
 
-				var clickHandler = function() {
-					
-					//e.preventDefault();
-					if( contextMenu ) {
-						if( contextMenu.hasClass('ui-menu') ) {
-							contextMenu.menu('destroy')
-						}
-						contextMenu.remove();
-					} 
+                contextMenu = $menu;
 
-					contextMenu = null;
-					$(document).unbind('click', clickHandler);
-				}
+                var clickHandler = function () {
 
-				var rightClickHandler = function() {
-					
-					//e.preventDefault();
-					if( contextMenu ) {
-						if( contextMenu.hasClass('ui-menu') ) {
-							contextMenu.menu('destroy')
-						}
-						contextMenu.remove();
-					} 
+                    //e.preventDefault();
+                    if (contextMenu) {
+                        if (contextMenu.hasClass('ui-menu')) {
+                            contextMenu.menu('destroy')
+                        }
+                        contextMenu.remove();
+                    }
 
-					contextMenu = null;
-				}
-				
-				$(document).bind('click', clickHandler);
-		//		return false;
+                    contextMenu = null;
+                    $(document).unbind('click', clickHandler);
+                }
 
-			}, true);
+                var rightClickHandler = function () {
 
+                    //e.preventDefault();
+                    if (contextMenu) {
+                        if (contextMenu.hasClass('ui-menu')) {
+                            contextMenu.menu('destroy')
+                        }
+                        contextMenu.remove();
+                    }
 
-			dom.parentNode.addEventListener('contextmenu', function(e) {
-				
+                    contextMenu = null;
+                }
 
-				//contextMenu.height(contextMenu.height(document.documentElement.clientHeight))
+                $(document).bind('click', clickHandler);
+                //		return false;
 
-				//console.log( contextMenu );
-				if( contextMenu.children().length > 0 ) {
-					contextMenu.menu({
-						select: function(event, ui) {
-							var moduleName = ui.item.attr('name');
-						}
-					});
-
-					e.preventDefault();
-					e.stopPropagation();
-					
-									// Move the menu if it would go beyond the viewport
-				var height = contextMenu.height();
-				var width = contextMenu.width();
-				var clientH = document.documentElement.clientHeight;
-				var clientW = document.documentElement.clientWidth;
-				if(top+height>clientH) {
-					contextMenu.css("top", Math.max(0, clientH-height-10));
-				}
-				if(left+width>clientW) {
-					contextMenu.css("left", Math.max(0, clientW-width-10));
-				}
-					
-					return false;
-				}
-				
-			}, false);
-		}
+            }, true);
 
 
-	}
+            dom.parentNode.addEventListener('contextmenu', function (e) {
+
+
+                //contextMenu.height(contextMenu.height(document.documentElement.clientHeight))
+
+                //console.log( contextMenu );
+                if (contextMenu.children().length > 0) {
+                    contextMenu.menu({
+                        select: function (event, ui) {
+                            var moduleName = ui.item.attr('name');
+                        }
+                    });
+
+                    e.preventDefault();
+                    e.stopPropagation();
+
+                    // Move the menu if it would go beyond the viewport
+                    var height = contextMenu.height();
+                    var width = contextMenu.width();
+                    var clientH = document.documentElement.clientHeight;
+                    var clientW = document.documentElement.clientWidth;
+                    if (top + height > clientH) {
+                        contextMenu.css("top", Math.max(0, clientH - height - 10));
+                    }
+                    if (left + width > clientW) {
+                        contextMenu.css("left", Math.max(0, clientW - width - 10));
+                    }
+
+                    return false;
+                }
+
+            }, false);
+        }
+
+
+    }
 });
