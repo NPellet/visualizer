61ebad23638363d0cc3ae86d9d4b2c4944599725
Michaël Zasso <mic.besace@gmail.com>
2015-02-25 13:31:32 +0100

    rich_text: use ckeditor for readOnly mode
    fixes #389

--- src/modules/types/edition/rich_text/view.js
+++ src/modules/types/edition/rich_text/view.js
@@ -21,11 +21,7 @@ define([
         },
         blank: {
             html: function () {
-                if (this.isEditable) {
-                    this.updateEditor('');
-                } else {
-                    this.updateHtml('');
-                }
+                this.updateEditor('');
             }
         },
         update: {
@@ -33,53 +29,43 @@ define([
                 this.module.data = moduleValue;
                 var val = moduleValue.get();
                 this.module.definition.richtext = val;
-                if (this.isEditable) {
-                    this.updateEditor(val);
-                } else {
-                    this.updateHtml(val);
-                }
+                this.updateEditor(val);
             }
         },
         initEditor: function () {
             var self = this;
             var initText = this.module.definition.richtext || '';
-            this.isEditable = this.module.getConfigurationCheckbox('editable', 'isEditable');
-            if (!this.isEditable) {
-                this.dom = $('<div id="' + self._id + '">');
-                this.module.getDomContent().html(self.dom);
-                this._setCss();
-                this.updateHtml(initText);
-            } else {
-                this.dom = $(' <div id="' + this._id + '" contenteditable="true">');
-                this.dom.html(initText);
-                this.module.getDomContent().html(this.dom);
-                this._setCss();
-                this.module.controller.valueChanged(initText);
-            }
+            this.readOnly = !this.module.getConfigurationCheckbox('editable', 'isEditable');
+            this.dom = $(' <div id="' + this._id + '" contenteditable="true">');
+            this.dom.html(initText);
+            this.module.getDomContent().html(this.dom);
+            this._setCss();
+            this.module.controller.valueChanged(initText);
             if (CKEDITOR.instances[this._id]) {
                 CKEDITOR.instances[this._id].destroy();
             }
-            if (this.module.getConfigurationCheckbox('editable', 'isEditable')) {
-                CKEDITOR.disableAutoInline = true;
-                this.instance = CKEDITOR.inline(this._id, {
-                    extraPlugins: 'mathjax,font,sourcedialog,codesnippet',
-                    removeButtons: ''
-                });
-                this.instance.on('change', function () {
-                    self.module.controller.valueChanged(self.instance.getData());
-                    if (self.module.getConfigurationCheckbox('autoHeight', 'yes')) {
-                        self.module.getDomWrapper().height(self.getContentHeight() + 50);
-                        Grid.moduleResize(self.module);
-                    }
-
-                });
+            CKEDITOR.disableAutoInline = true;
+            var options = {
+                extraPlugins: 'mathjax,font,sourcedialog,codesnippet',
+                removeButtons: ''
+            };
+            if (this.readOnly) {
+                options.readOnly = true;
+                options.removePlugins = 'toolbar';
+                options.allowedContent = true;
             }
+            this.instance = CKEDITOR.inline(this._id, options);
+            this.instance.on('change', function () {
+                self.module.controller.valueChanged(self.instance.getData());
+                if (self.module.getConfigurationCheckbox('autoHeight', 'yes')) {
+                    self.module.getDomWrapper().height(self.getContentHeight() + 50);
+                    Grid.moduleResize(self.module);
+                }
+
+            });
         },
         updateEditor: function (html) {
             this.instance.setData(html);
-        },
-        updateHtml: function (html) {
-            this.dom.html(html);
         },
         getContentHeight: function () {
             var height = 0;
