039b41bb67e1c9f6f7d5668212ac3201b6ad5a69
Michaël Zasso <mic.besace@gmail.com>
2015-04-02 16:42:17 +0200

    2d_list: clean code

--- src/modules/types/display/2d_list/view.js
+++ src/modules/types/display/2d_list/view.js
@@ -1,167 +1,162 @@
-define([ 'modules/default/defaultview', 'src/util/typerenderer', 'src/util/api' ], function( Default, Renderer, API ) {
-	
-	function view() {};
-	view.prototype = $.extend(true, {}, Default, {
+'use strict';
 
-		init: function() {	
-			var html = [];
-			html.push( '<div class="ci-displaylist-list-2d"></div>' );
-			this.dom = $( html.join('') );
-			this.module.getDomContent().html( this.dom );
+define(['modules/default/defaultview', 'src/util/typerenderer', 'src/util/api'], function (Default, Renderer, API) {
 
-		},
-                
+    function View() {
+    }
+
+    View.prototype = $.extend(true, {}, Default, {
+
+        init: function () {
+            var html = [];
+            html.push('<div class="ci-displaylist-list-2d"></div>');
+            this.dom = $(html.join(''));
+            this.module.getDomContent().html(this.dom);
+
+        },
         blank: {
-            list: function() {
-                API.killHighlight( this.module.getId() );
+            list: function () {
+                API.killHighlight(this.module.getId());
                 this.dom.empty();
             }
         },
-		
-		inDom: function() {
-			var self = this;
-			this.module.getDomView().on('mouseenter mouseleave click', 'td', function(e) {
-				var tdIndex = $(this).index();
-				var trIndex = $(this).parent().index();
-				var cols = self.module.getConfiguration('colnumber', 4) || 4;
-				var elementId = trIndex * cols + tdIndex;
-				var value = self.list.get()[elementId];
-               if(e.type === "mouseenter") {
-               		self.module.controller.setVarFromEvent( 'onHover', 'cell', 'list', [ elementId ] );
+        inDom: function () {
+            var self = this;
+            this.module.getDomView().on('mouseenter mouseleave click', 'td', function (e) {
+                var tdIndex = $(this).index();
+                var trIndex = $(this).parent().index();
+                var cols = self.module.getConfiguration('colnumber', 4) || 4;
+                var elementId = trIndex * cols + tdIndex;
+                var value = self.list.get()[elementId];
+                if (e.type === "mouseenter") {
+                    self.module.controller.setVarFromEvent('onHover', 'cell', 'list', [elementId]);
                     API.highlight(value, 1);
                 }
-                else if(e.type === "mouseleave") {
+                else if (e.type === "mouseleave") {
                     API.highlight(value, 0);
                 }
-                else if(e.type === "click") {
-                    self.module.controller.setVarFromEvent( 'onClick', 'cell', 'list', [ elementId ] );
+                else if (e.type === "click") {
+                    self.module.controller.setVarFromEvent('onClick', 'cell', 'list', [elementId]);
                     self.module.controller.sendAction('cell', value, 'onClick');
                 }
-			});
-			this.resolveReady();
-		},
+            });
+            this.resolveReady();
+        },
 
-		update: {
+        update: {
 
-			list: function( moduleValue ) {
+            list: function (moduleValue) {
 
-				this.defs = [];
-				if(!Array.isArray(moduleValue)) {
-					return;
-				}
+                this.defs = [];
+                if (!Array.isArray(moduleValue)) {
+                    return;
+                }
 
-				var view = this,
-					cfg = $.proxy( this.module.getConfiguration, this.module ),
-					cols = cfg('colnumber', 4) || 4,
-					sizeStyle = "",
-					self = this,
-					val = moduleValue.get(),
-					table = $('<table cellpadding="3" cellspacing="0">').css("text-align", "center"),
-					l = val.length,
-					done = 0,
-					td,
-					i = 0;
+                var view = this,
+                    cfg = $.proxy(this.module.getConfiguration, this.module),
+                    cols = cfg('colnumber', 4) || 4,
+                    sizeStyle = "",
+                    self = this,
+                    val = moduleValue.get(),
+                    table = $('<table cellpadding="3" cellspacing="0">').css("text-align", "center"),
+                    l = val.length,
+                    done = 0,
+                    td,
+                    i = 0;
 
-				self.list = val;
+                self.list = val;
 
-				if( cfg.width || cfg.height ) {
+                if (cfg.width || cfg.height) {
 
-					if( cfg.width ) {
+                    if (cfg.width) {
 
-						sizeStyle += "width: " + Math.round(100 / cols) + "%; ";
-					}
+                        sizeStyle += "width: " + Math.round(100 / cols) + "%; ";
+                    }
 
-					if( cfg.height ) {
+                    if (cfg.height) {
 
-						sizeStyle += "height: " + cfg.height + "px; ";
-					}
-				}
+                        sizeStyle += "height: " + cfg.height + "px; ";
+                    }
+                }
 
-				var current, colId;
-				this._inDom = false;
-				
-				for( ; i < l ; i ++ ) {
+                var current, colId;
+                this._inDom = false;
 
-					td = this.renderElement( view.list.getChildSync( [i] ), cols );
-					colId = done % cols;
+                for (; i < l; i++) {
 
-					if( colId === 0 ) {
-						if( current ) {
-							current.appendTo( table );
-						}
-						current = $( "<tr />" );
-					}
+                    td = this.renderElement(view.list.getChildSync([i]), cols);
+                    colId = done % cols;
 
-					done++;
-					td.appendTo( current );
-				}
+                    if (colId === 0) {
+                        if (current) {
+                            current.appendTo(table);
+                        }
+                        current = $("<tr />");
+                    }
 
-				if( current ) {
-					current.appendTo( table );
-				}
-				
-				view.dom.html( table );
+                    done++;
+                    td.appendTo(current);
+                }
 
-				i = 0;
-				l = this.defs.length;
+                if (current) {
+                    current.appendTo(table);
+                }
 
-				for( ; i < l ; i ++ ) {
-					
-					( function( j ) {
+                view.dom.html(table);
 
-						self.defs[ j ].then( function() {
-							if( self.defs[ j ].build ) {
-								self.defs[ j ].build( );
-							}
-						});
+                i = 0;
+                l = this.defs.length;
 
-					} ) ( i );
-				}				
-			}
-		},
+                for (; i < l; i++) {
 
-		renderElement: function(element, cols) {
+                    (function (j) {
 
-			var cfg = $.proxy( this.module.getConfiguration, this.module ),
-				colorJpath = cfg('colorjpath', false),
-				valJpath = cfg('valjpath', ''),
-				td = $( "<td>" ).css( {
-					width: Math.round(100 / cols) + "%", 
-					height: cfg.height
-				} );
-
-			if( colorJpath ) {
-
-				element.getChild( colorJpath , true ).then( function( val ) {
-					td.css( 'background-color', val.get() );
-				} );
-			}
-
-			this.defs.push( Renderer.toScreen( element, this.module, { }, valJpath ).always( function(val) {
-
-				td.html(val);
-
-			} ) );
-                        
-                        API.listenHighlight( element, function( onOff, key ) {
-                            if(onOff) {
-                                td.css("border-color", "black");
-                            } else {
-                                td.css("border-color", "");
+                        self.defs[j].then(function () {
+                            if (self.defs[j].build) {
+                                self.defs[j].build();
                             }
-			}, false, this.module.getId());
-			
-			return td;
-		},
+                        });
 
-		getDom: function() {
-			return this.dom;
-		},
-		
-		typeToScreen: { }
-	});
+                    })(i);
+                }
+            }
+        },
 
-	return view;
+        renderElement: function (element, cols) {
+
+            var cfg = $.proxy(this.module.getConfiguration, this.module),
+                colorJpath = cfg('colorjpath', false),
+                valJpath = cfg('valjpath', ''),
+                td = $("<td>").css({
+                    width: Math.round(100 / cols) + "%",
+                    height: cfg.height
+                });
+
+            if (colorJpath) {
+
+                element.getChild(colorJpath, true).then(function (val) {
+                    td.css('background-color', val.get());
+                });
+            }
+
+            this.defs.push(Renderer.toScreen(element, this.module, {}, valJpath).always(function (val) {
+
+                td.html(val);
+
+            }));
+
+            API.listenHighlight(element, function (onOff, key) {
+                if (onOff) {
+                    td.css("border-color", "black");
+                } else {
+                    td.css("border-color", "");
+                }
+            }, false, this.module.getId());
+
+            return td;
+        }
+    });
+
+    return View;
+
 });
-
-
