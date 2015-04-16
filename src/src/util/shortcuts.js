7f3ce0a3fe7dec8994457c9ea5c510fdd9f48213
stropitek <kostro.d@gmail.com>
2015-04-16 11:33:37 +0200

    Add numpad ctrl+/ to searchbox shortcut

--- src/src/util/shortcuts.js
+++ src/src/util/shortcuts.js
@@ -2,7 +2,7 @@ define([], function() {
         $(document).keydown(function(event) {
                 // If Control or Command key is pressed and the S key is pressed
                 // run save function. 83 is the key code for S.
-                if((event.ctrlKey || event.metaKey) && event.which == 191) {
+                if((event.ctrlKey || event.metaKey) && (event.which == 191 || event.which === 111)) {
                     require(['src/util/searchModule'], function(searchModule) {
                         searchModule();
                     });
