2f6bd358c6b3e4a0c139d9d7997bcc9a6e3e651b
Michaël Zasso <mic.besace@gmail.com>
2015-04-20 21:49:24 +0200

    single_value: fix html rendering of default value
    closes #500

--- src/modules/types/display/single_value/view.js
+++ src/modules/types/display/single_value/view.js
@@ -37,7 +37,7 @@ define([
 
             this.values = {};
             this.module.getDomContent().html(this.dom);
-            this.fillWithVal(this.module.getConfiguration('defaultvalue', ''));
+            this.fillWithVal({type:'html', value:this.module.getConfiguration('defaultvalue', '')});
             this.resolveReady();
             this._relsForLoading = ['value'];
         },
