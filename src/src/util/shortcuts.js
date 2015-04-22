b5ba7ab6214871c21ec5e2502b03c6dd51d0b3c3
stropitek <kostro.d@gmail.com>
2015-04-22 15:42:55 +0200

    shortcut: prevent default

--- src/src/util/shortcuts.js
+++ src/src/util/shortcuts.js
@@ -3,6 +3,7 @@ define([], function() {
                 // If Control or Command key is pressed and the S key is pressed
                 // run save function. 83 is the key code for S.
                 if((event.ctrlKey || event.metaKey) && (event.which == 191 || event.which === 111)) {
+                    event.preventDefault();
                     require(['src/util/searchModule'], function(searchModule) {
                         searchModule();
                     });
