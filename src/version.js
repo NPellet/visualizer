1131efcf7a732e5716cfc4389819fefe285bc1e1
stropitek <kostro.d@gmail.com>
2015-04-29 11:29:46 +0200

    Release v2.18.0

--- src/version.js
+++ src/version.js
@@ -4,10 +4,10 @@ define(['lib/semver/semver'], function (semver) {
 
     /* DO NOT MODIFY THE FOLLOWING LINES MANUALLY */
     var MAJOR = 2;
-    var MINOR = 17;
-    var PATCH = 9;
-    var PRERELEASE = 0;
-    var IS_RELEASE = false;
+    var MINOR = 18;
+    var PATCH = 0;
+    var PRERELEASE = false;
+    var IS_RELEASE = true;
     var BUILD_TIME = null;
     /* END */
 
