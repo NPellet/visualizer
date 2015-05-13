6ecf539a773eb362cf3ba64ef2d0b52f7d4d8095
Michaël Zasso <mic.besace@gmail.com>
2015-05-13 14:40:31 +0200

    lint: add no-irregular-whitespace rule

--- src/modules/types/science/spectra/nmr/2dnmr/view.js
+++ src/modules/types/science/spectra/nmr/2dnmr/view.js
@@ -17,52 +17,16 @@ define([
         },
 
         inDom: function () {
-
-            var nmr = new NMR({
+            this.nmr = new NMR({
                 dom: this.dom,
                 mode: '2d',
                 symmetric: true,
                 minimap: false
             });
-
-            this.nmr = nmr;
-
-            /*    nmr.load( {
-
-             urls: {
-             twoD: 'components/jsNMR/test/cosy/84-74-2_cosygpppqf.jdx',
-             x: 'components/jsNMR/test/cosy/84-74-2_zg.jdx'
-             },
-
-             lineColor: 'rgb(' + hslToRgb( 100 / 360, 0.8, 0.4 ).join() + ')',
-             twoDColor: {
-
-             fromPositive: { h: 100, s: 0.3, l: 0.7 },
-             toPositive: { h: 100, s: 1, l: 0.5},
-
-             fromNegative: { h: 100, s: 0.3, l: 0.5  },
-             toNegative: { h: 100, s: 1, l: 0.3 }
-             },
-             label: 'Chemical 1'
-             });
-             */
-
         },
 
         onResize: function () {
-
             this.nmr.resize2DTo(this.width, this.height);
-            /*
-             if (this.nmr.graphs['_2d']) {
-             this.nmr.graphs['_2d'].resize(this.width - 160, this.height - 160);
-             }
-             if (this.nmr.graphs['x']) {
-             this.nmr.graphs['x'].resize(this.width - 160, 150);
-             }
-             if (this.nmr.graphs['y']) {
-             this.nmr.graphs['y'].resize(150, this.height - 160);
-             }
-             this.redraw();*/
         },
 
         blank: {
