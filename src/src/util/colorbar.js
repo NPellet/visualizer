e61c601bcfcb1e3d4a071d387ce76e61b2c4eb45
Daniel Kostro <kostro.d@gmail.com>
2015-05-19 16:27:01 +0200

    new util: colorbar

---
+++ src/src/util/colorbar.js
@@ -0,0 +1,101 @@
+define(['d3', 'src/util/util'], function(d3, Util) {
+    var exports = {};
+
+    Util.loadCss('src/util/colorbar.css');
+
+    exports.getSvg = function(options) {
+        var el = document.createElement('div');
+        return exports.renderSvg(el, options);
+    };
+
+    exports.renderSvg = function(el, options) {
+        var linearg = getGradientXY(options.axis.orientation);
+        var margin = 30;
+        var totalWidth = options.width + margin;
+        var totalHeight = options.height + margin;
+        var svg = d3.select(el).append('svg')
+            .attr('width', totalWidth)
+            .attr('height', totalHeight);
+        svg.append('defs').append('linearGradient')
+            .attr({
+                x1: linearg[0],
+                y1: linearg[1],
+                x2: linearg[2],
+                y2: linearg[3],
+                id: 'grad1'
+            })
+            .selectAll('stop')
+            .data(options.stops)
+            .enter().append('stop')
+            .attr('offset', function(d, i) {
+                return '' + (options.stopPositions[i]*100) + '%';
+            })
+            .style('stop-color', function(d, i) {
+                return d;
+            })
+            .style('stop-opacity', 1);
+
+        var g = svg.append('g')
+            .attr('class', 'key')
+            .attr('transform', function() {
+                var tx = 0;
+                var ty = 0;
+                var r = 'translate(';
+                if(options.axis.orientation === 'left' || options.axis.orientation === 'right') {
+                    ty += margin/2;
+                }
+                else if(options.axis.orientation === 'bottom' || options.axis.orientation === 'top') {
+                    tx += margin/2;
+                }
+                if(options.axis.orientation === 'left') {
+                    tx += margin;
+                }
+                if(options.axis.orientation === 'top') {
+                    ty += margin;
+                }
+                return 'translate(' + tx +',' + ty + ')';
+            });
+        g.append('rect')
+            .style('fill', 'url(#grad1)')
+            .attr('width', options.width)
+            .attr('height', options.height);
+
+
+
+        var x = d3.scale.linear()
+            .domain(options.domain).nice()
+            .range([0, (options.axis.orientation === 'bottom' || options.axis.orientation === 'top' ? options.width: options.height )]);
+        var axis = d3.svg.axis()
+            .scale(x)
+            .orient(options.axis.orientation)
+            .tickSize(6)
+            .ticks(options.axis.ticks);
+        g.append('g')
+            .attr('class', 'key')
+            .attr('transform', function() {
+                var tx=0, ty=0;
+                if(options.axis.orientation === 'bottom') {
+                    ty += options.height;
+                }
+                else if(options.axis.orientation === 'right') {
+                    tx += options.width;
+                }
+                return 'translate(' + tx + ',' + ty + ')';
+            })
+            .call(axis);
+
+        return svg.html();
+    };
+
+    function getGradientXY(orientation) {
+        if(orientation === 'left' || orientation === 'right') {
+            return ['0%','0%', '0%', '100%'];
+        }
+        else {
+            return ['0%','0%','100%','0%'];
+        }
+
+    }
+
+    return exports;
+});
