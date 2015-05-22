bd16e45da661f4ef1e855706cb7a33db1ac92d9e
Daniel Kostro <kostro.d@gmail.com>
2015-05-22 13:54:53 +0200

    shortcut: don't accept search module shortcut if alt key pressed

--- src/src/util/shortcuts.js
+++ src/src/util/shortcuts.js
@@ -4,7 +4,7 @@ define(['jquery'], function ($) {
     $(document).keydown(function (event) {
             // If Control or Command key is pressed and the S key is pressed
             // run save function. 83 is the key code for S.
-            if ((event.ctrlKey || event.metaKey) && (event.which == 191 || event.which === 111)) {
+            if ((event.ctrlKey || event.metaKey) && !event.altKey && (event.which == 191 || event.which === 111)) {
                 event.preventDefault();
                 require(['src/util/searchModule'], function (searchModule) {
                     searchModule();
