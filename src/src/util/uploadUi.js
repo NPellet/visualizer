7e60a10218d6ed7cdae5f77b92c70853451c9871
Daniel Kostro <kostro.d@gmail.com>
2015-07-03 16:23:38 +0200

    uploadUi: revert breaking change

--- src/src/util/uploadUi.js
+++ src/src/util/uploadUi.js
@@ -124,24 +124,24 @@ define([
                         open: function () {
                             $dialog.append($slick);
                             //$('body').append($slick);
-                            grid = new Slick.Grid($slick, slickData, columns, slickOptions);
-                            grid.onSort.subscribe(function (e, args) {
-                                var cols = args.sortCols;
-                                slickData.sort(function (dataRow1, dataRow2) {
-                                    for (var i = 0, l = cols.length; i < l; i++) {
-                                        var field = cols[i].sortCol.field;
-                                        var sign = cols[i].sortAsc ? 1 : -1;
-                                        var value1 = dataRow1[field], value2 = dataRow2[field];
-                                        var result = (value1 == value2 ? 0 : (value1 > value2 ? 1 : -1)) * sign;
-                                        if (result != 0) {
-                                            return result;
-                                        }
-                                    }
-                                    return 0;
-                                });
-                                grid.invalidate();
-                                grid.render();
-                            });
+                            grid = new Slick.Grid($slick, data, columns, slickOptions);
+                            //grid.onSort.subscribe(function (e, args) {
+                            //    var cols = args.sortCols;
+                            //    slickData.sort(function (dataRow1, dataRow2) {
+                            //        for (var i = 0, l = cols.length; i < l; i++) {
+                            //            var field = cols[i].sortCol.field;
+                            //            var sign = cols[i].sortAsc ? 1 : -1;
+                            //            var value1 = dataRow1[field], value2 = dataRow2[field];
+                            //            var result = (value1 == value2 ? 0 : (value1 > value2 ? 1 : -1)) * sign;
+                            //            if (result != 0) {
+                            //                return result;
+                            //            }
+                            //        }
+                            //        return 0;
+                            //    });
+                            //    grid.invalidate();
+                            //    grid.render();
+                            //});
                         },
                         closeOnEscape: false,
                         width: 700,
