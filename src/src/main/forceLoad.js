737decbd15b7cd16d297c0ddb597eea96ff1cb8c
Michaël Zasso <mic.besace@gmail.com>
2016-02-12 16:19:15 +0100

    do not preload modules

--- src/src/main/forceLoad.js
+++ src/src/main/forceLoad.js
@@ -2,7 +2,6 @@
 
 // force loading of important dynamic files to put them in the build
 define([
-    // headers
     'src/header/components/blankview',
     'src/header/components/copydata',
     'src/header/components/copyview',
@@ -17,31 +16,7 @@ define([
 
     'modules/default/defaultcontroller',
     'modules/default/defaultmodel',
-    'modules/default/defaultview',
-
-    // code executor loads the ace editor. This is problematic for dynamic loading of syntax coloring files
-    //'modules/types/client_interaction/code_executor/controller',
-    //'modules/types/client_interaction/code_executor/model',
-    //'modules/types/client_interaction/code_executor/view',
-    'modules/types/display/single_value/controller',
-    'modules/types/display/single_value/model',
-    'modules/types/display/single_value/view',
-    'modules/types/edition/slick_grid/controller',
-    'modules/types/edition/slick_grid/model',
-    'modules/types/edition/slick_grid/view',
-
-    //'lib/forms/fieldelement',
-    //'lib/forms/fieldlistelement',
-    //'lib/forms/types/checkbox/element',
-    //'lib/forms/types/checkbox/list',
-    //'lib/forms/types/checkbox/table',
-    //'lib/forms/types/combo/element',
-    //'lib/forms/types/combo/table',
-    //'lib/forms/types/jscode/element',
-    //'lib/forms/types/jscode/list',
-    //'lib/forms/types/text/element',
-    //'lib/forms/types/text/list',
-    //'lib/forms/types/text/table'
+    'modules/default/defaultview'
 ], function () {
     return {};
 });
