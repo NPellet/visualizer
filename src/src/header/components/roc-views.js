1784286e60a827e5cdb77044e10e7f7d7eb7f427
Daniel Kostro <kostro.d@gmail.com>
2016-02-24 16:40:45 +0100

    renderFlavor: check if is folder

--- src/src/header/components/roc-views.js
+++ src/src/header/components/roc-views.js
@@ -758,7 +758,7 @@ define([
         renderFlavor() {
             var found = false;
             this.tree.filterBranches(node => {
-                if (node.title === this.flavor && node.data.path.length === 1) {
+                if (node.folder && node.title === this.flavor && node.data.path.length === 1) {
                     found = true;
                     node.setExpanded(true);
                     return true;
