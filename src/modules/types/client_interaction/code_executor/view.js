bf8521fffb4c7f131f33c0922f05fa1e974eb844
Daniel Kostro <kostro.d@gmail.com>
2016-05-06 14:24:20 +0200

    code_executor: set higher height limit for not displaying code

--- src/modules/types/client_interaction/code_executor/view.js
+++ src/modules/types/client_interaction/code_executor/view.js
@@ -69,7 +69,7 @@ define(['modules/default/defaultview', 'src/util/util', 'ace/ace', 'src/util/con
     View.prototype.onResize = function () {
         if (this.editor) {
             var $editor = $('#' + this._id);
-            if ($editor.height() < 20) {
+            if ($editor.height() < 30) {
                 $editor.hide();
             } else {
                 $editor.show();
