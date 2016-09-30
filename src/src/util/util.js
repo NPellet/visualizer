7a952cf25675644bea28711b6c22d041851e13c8
Michaël Zasso <targos@protonmail.com>
2016-09-30 09:42:44 +0200

    refactor(util): use CSS animations for loading SVG

--- src/src/util/util.js
+++ src/src/util/util.js
@@ -384,37 +384,41 @@ define([
         if (size === undefined) size = 32;
         if (color === undefined) color = 'black';
         // Image taken from https://github.com/jxnblk/loading (loading-bars.svg)
-        var $elem = $('\
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="black">\
-            <rect x="0" y="12" width="4" height="8"></rect>\
-            <rect x="7" y="12" width="4" height="8"></rect>\
-            <rect x="14" y="12" width="4" height="8"></rect>\
-            <rect x="21" y="12" width="4" height="8"></rect>\
-            <rect x="28" y="12" width="4" height="8"></rect>\
-        </svg>').attr({
+        var $elem = $(`
+ <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="black">
+   <style>
+     @keyframes ci-loading-rectangles {
+       0% {transform: none}
+       25% { transform: matrix(1,0,0,2,0,-16) }
+       50% { transform: none}
+     }
+     .ci-loading-rectanim {
+       animation: ci-loading-rectangles 1s infinite
+     }
+     #ci-loading-rectanim-2 {
+       animation-delay: 0.16s;
+     }
+     #ci-loading-rectanim-3 {
+       animation-delay: 0.32s;
+     }
+     #ci-loading-rectanim-4 {
+       animation-delay: 0.48s;
+     }
+     #ci-loading-rectanim-5 {
+       animation-delay: 0.64s;
+     }
+   </style>
+   <rect class="ci-loading-rectanim" id="ci-loading-rectanim-1" x="0" y="12" width="4" height="8"></rect>
+   <rect class="ci-loading-rectanim" id="ci-loading-rectanim-2" x="7" y="12" width="4" height="8"></rect>
+   <rect class="ci-loading-rectanim" id="ci-loading-rectanim-3" x="14" y="12" width="4" height="8"></rect>
+   <rect class="ci-loading-rectanim" id="ci-loading-rectanim-4" x="21" y="12" width="4" height="8"></rect>
+   <rect class="ci-loading-rectanim" id="ci-loading-rectanim-5" x="28" y="12" width="4" height="8"></rect>
+</svg>
+`).attr({
             width: size,
             height: size,
             fill: color
         });
-
-        var delay = 0;
-        var duration = 1000;
-
-        $elem.find('rect').each(function () {
-            this.animate([
-                {transform: 'scale(1,1)'},
-                {transform: 'matrix(1,0,0,2,0,-16)'},
-                {transform: 'scale(1,1)'},
-                {transform: 'scale(1,1)'},
-                {transform: 'scale(1,1)'}
-            ], {
-                duration: duration,
-                iterations: Infinity,
-                delay: delay
-            });
-            delay += duration / 6;
-        });
-
         return $elem;
     };
 
