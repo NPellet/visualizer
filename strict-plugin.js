073aa639c6e2eb41f6bbdcf9c8fd9d4f8bd0e85f
Daniel Kostro <kostro.d@gmail.com>
2017-01-19 17:08:50 +0100

    refactor: clean strict babel plugin

--- strict-plugin.js
+++ strict-plugin.js
@@ -5,16 +5,11 @@ const t = require('babel-types');
 module.exports = function () {
     return {
         visitor: {
-            CallExpression(path, state) {
+            CallExpression(path) {
                 const spath = path.getPathLocation().split('.');
                 if (spath.length === 3 && spath[0] === 'program' && path.node.callee.name === 'define') {
                     path.traverse({
-                        FunctionDeclaration(path, state) {
-                            // if (path.getStatementParent().getPathLocation().split('.').length === 2) {
-                            //     debugger;
-                            // }
-                        },
-                        Function(path, state) {
+                        Function(path) {
                             const spath = path.getPathLocation().split('.');
                             if (spath.length === 4 && spath[3].startsWith('arguments')) {
                                 path.get('body').unshiftContainer('body', t.expressionStatement(t.stringLiteral('use strict')));
@@ -22,18 +17,6 @@ module.exports = function () {
                         }
                     });
                 }
-                return;
-                if (state.opts.strict === false || state.opts.strictMode === false) return;
-
-                if (path.parent.node)
-                    return;
-                const {node} = path;
-
-                // for (const directive of (node.directives: Array<Object>)) {
-                //     if (directive.value.value === "use strict") return;
-                // }
-
-                path.unshiftContainer("directives", t.directive(t.directiveLiteral("use strict")));
             }
         }
     };
