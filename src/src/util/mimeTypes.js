118ad93e4fd27db08d465de49727c326446dba0a
Daniel Kostro <kostro.d@gmail.com>
2017-05-10 15:01:20 +0200

    add genbank and fasta mime types

--- src/src/util/mimeTypes.js
+++ src/src/util/mimeTypes.js
@@ -2,10 +2,20 @@
 
 define(['mime-types'], function (mimeTypes) {
     return {
-        lookup: function (filename) {
+        lookup: function (filename, override) {
             var contentType = mimeTypes.lookup(filename);
-            if (!contentType && /\.j?dx$/i.test(filename)) {
-                contentType = 'chemical/x-jcamp-dx';
+            if (!contentType || override) {
+                if (/\.j?dx$/i.test(filename)) {
+                    contentType = 'chemical/x-jcamp-dx';
+                } else if (/\.gbk?$/i.test(filename)) {
+                    contentType = 'chemical/seq-na-genbank';
+                } else if (/\.genbank$/i.test(filename)) {
+                    contentType = 'chemical/seq-na-genbank';
+                } else if (/\.(fasta|fa|fna)$/i.test(filename)) { // is fsa nucleic or amino acid?
+                    contentType = 'chemical/seq-na-fasta';
+                } else if (/\.(mpfa|faa)$/i.test(filename)) {
+                    contentType = 'chemical/seq-aa-fasta';
+                }
             }
             return contentType;
         }
