0ef68827650f517c0e870a559a142f3b8c9483c2
lpatiny <gb252243>
2017-07-02 16:08:32 +0200

    Release v2.82.3

--- src/version.js
+++ src/version.js
@@ -6,8 +6,8 @@ define(['lib/semver/semver'], function (semver) {
     const MAJOR = 2;
     const MINOR = 82;
     const PATCH = 3;
-    const PRERELEASE = 0;
-    const IS_RELEASE = false;
+    const PRERELEASE = false;
+    const IS_RELEASE = true;
     const BUILD_TIME = null;
     const INCLUDED_MODULE_CSS = [];
     /* END */
