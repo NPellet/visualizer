ea5624ff1d56f49444061aa604afb226a9456c2f
Daniel Kostro <kostro.d@gmail.com>
2018-06-20 15:47:14 +0200

    allow icons to have colors in module header

--- src/modules/default/defaultcontroller.js
+++ src/modules/default/defaultcontroller.js
@@ -1,309 +1,314 @@
 'use strict';
 
-define(['jquery', 'src/util/api', 'src/util/util', 'src/main/grid'], function (
-    $,
-    API,
-    Util,
-    Grid
-) {
-    return {
-        setModule(module) {
-            this.module = module;
-        },
+define(
+    [
+        'jquery',
+        'src/util/api',
+        'src/util/util',
+        'src/util/color',
+        'src/main/grid'
+    ],
+    function ($, API, Util, Color, Grid) {
+        return {
+            setModule(module) {
+                this.module = module;
+            },
 
-        init() {
-            this.initImpl();
-        },
+            init() {
+                this.initImpl();
+            },
 
-        initImpl() {
-            this.resolveReady();
-        },
+            initImpl() {
+                this.resolveReady();
+            },
 
-        getToolbar() {
-            var tb = this.module.definition.toolbar;
-            if (tb) {
-                var common = this.module.definition.toolbar.common[0]
-                    .toolbar[0];
-                var custom = this.module.definition.toolbar.custom[0];
-            }
-
-            if (!common) common = ['Open Preferences'];
-
-            var toolbar = [
-                {
-                    onClick() {
-                        this.exportData();
-                    },
-                    title: 'Export Data',
-                    cssClass: 'fa fa-sign-out',
-                    ifLocked: true
-                },
-                {
-                    onClick() {
-                        this.printView();
-                    },
-                    title: 'Print',
-                    cssClass: 'fa fa-print',
-                    ifLocked: true
-                },
-                {
-                    onClick() {
-                        this.enableFullscreen();
-                    },
-                    title: 'Show fullscreen',
-                    cssClass: 'fa fa-expand',
-                    ifLocked: true
-                },
-                {
-                    onClick() {
-                        this.doConfig(2);
-                    },
-                    title: 'Open Preferences',
-                    cssClass: 'fa fa-wrench',
-                    ifLocked: false
+            getToolbar() {
+                var tb = this.module.definition.toolbar;
+                if (tb) {
+                    var common = this.module.definition.toolbar.common[0]
+                        .toolbar[0];
+                    var custom = this.module.definition.toolbar.custom[0];
                 }
-                //{
-                //    onClick() {
-                //        this.doConfig(3);
-                //    },
-                //    title: "Variables in",
-                //    cssClass: 'fa fa-sign-in fa-lg',
-                //    ifLocked: true
-                //}
-                //{
-                //    onClick() {
-                //        this.doConfig(4)
-                //    },
-                //    title: "Variables out",
-                //    cssClass: 'fa fa-sign-out fa-lg'
-                //},
-                //{
-                //    onClick() {
-                //        Grid.removeModule(this);
-                //    },
-                //    title: "Remove module",
-                //    cssClass: 'fa fa-close fa-lg'
-                //}
-            ];
 
-            if (common) {
-                toolbar = toolbar.filter(t => {
-                    return common.some(c => {
-                        return c === t.title;
-                    });
-                });
-            }
+                if (!common) common = ['Open Preferences'];
 
-            const customB = [];
-            const customE = [];
-            if (custom) {
-                custom = custom.filter(el => el.title);
-                for (let i = 0; i < custom.length; i++) {
-                    let el = {
-                        ifLocked: true,
-                        title: custom[i].title,
-                        cssClass: 'fa ' + custom[i].icon,
-                        onClick: function () {
-                            API.doAction(custom[i].action);
-                        }
-                    };
-                    if (custom[i].position === 'begin') {
-                        customB.push(el);
-                    } else {
-                        customE.push(el);
+                var toolbar = [
+                    {
+                        onClick() {
+                            this.exportData();
+                        },
+                        title: 'Export Data',
+                        cssClass: 'fa fa-sign-out',
+                        ifLocked: true
+                    },
+                    {
+                        onClick() {
+                            this.printView();
+                        },
+                        title: 'Print',
+                        cssClass: 'fa fa-print',
+                        ifLocked: true
+                    },
+                    {
+                        onClick() {
+                            this.enableFullscreen();
+                        },
+                        title: 'Show fullscreen',
+                        cssClass: 'fa fa-expand',
+                        ifLocked: true
+                    },
+                    {
+                        onClick() {
+                            this.doConfig(2);
+                        },
+                        title: 'Open Preferences',
+                        cssClass: 'fa fa-wrench',
+                        ifLocked: false
                     }
-                }
-            }
+                    //{
+                    //    onClick() {
+                    //        this.doConfig(3);
+                    //    },
+                    //    title: "Variables in",
+                    //    cssClass: 'fa fa-sign-in fa-lg',
+                    //    ifLocked: true
+                    //}
+                    //{
+                    //    onClick() {
+                    //        this.doConfig(4)
+                    //    },
+                    //    title: "Variables out",
+                    //    cssClass: 'fa fa-sign-out fa-lg'
+                    //},
+                    //{
+                    //    onClick() {
+                    //        Grid.removeModule(this);
+                    //    },
+                    //    title: "Remove module",
+                    //    cssClass: 'fa fa-close fa-lg'
+                    //}
+                ];
 
-            return customB.concat(toolbar, customE);
-        },
-
-        inDom: Util.noop,
-
-        setVarFromEvent(event, rel, relSource, jpath, callback) {
-            const varsOut = this.module.vars_out();
-            if (!varsOut) {
-                return;
-            }
-
-            let first = true;
-            for (let i = 0; i < varsOut.length; i++) {
-                if (
-                    varsOut[i].event == event &&
-                    (varsOut[i].rel == rel || !rel) &&
-                    varsOut[i].name
-                ) {
-                    if (first && callback) {
-                        first = false;
-                        callback.call(this);
-                    }
-
-                    varsOut[i].jpath = varsOut[i].jpath || []; // Need not be undefined
-
-                    if (typeof varsOut[i].jpath == 'string') {
-                        varsOut[i].jpath = varsOut[i].jpath.split('.');
-                        varsOut[i].jpath.shift();
-                    }
-
-                    API.setVar(
-                        varsOut[i].name,
-                        this.module.getVariableFromRel(relSource),
-                        jpath.concat(varsOut[i].jpath),
-                        varsOut[i].filter
-                    );
-                }
-            }
-        },
-
-        createDataFromEvent(event, rel, data, callback) {
-            const varsOut = this.module.vars_out();
-            if (!varsOut) {
-                return;
-            }
-
-            let first = true;
-            for (let i = 0; i < varsOut.length; i++) {
-                if (
-                    varsOut[i].event == event &&
-                    (varsOut[i].rel == rel || !rel) &&
-                    varsOut[i].name
-                ) {
-                    if (first && callback) {
-                        first = false;
-                        data = callback.call(this);
-                    }
-
-                    API.createDataJpath(
-                        varsOut[i].name,
-                        data,
-                        varsOut[i].jpath,
-                        varsOut[i].filter
-                    );
-                }
-            }
-        },
-
-        sendActionFromEvent(event, rel, value) {
-            const actionsOut = this.module.actions_out();
-
-            if (!actionsOut) {
-                return;
-            }
-
-            for (let i = actionsOut.length - 1; i >= 0; i--) {
-                if (
-                    actionsOut[i].name &&
-                    actionsOut[i].rel === rel &&
-                    ((event && event === actionsOut[i].event) || !event)
-                ) {
-                    const actionName = actionsOut[i].name;
-                    const jpath = actionsOut[i].jpath;
-
-                    if (value && jpath) {
-                        if (!value.getChild) {
-                            value = DataObject.check(value, true);
-                        }
-
-                        value.getChild(jpath).then(returned => {
-                            API.doAction(actionName, returned);
+                if (common) {
+                    toolbar = toolbar.filter(t => {
+                        return common.some(c => {
+                            return c === t.title;
                         });
-                    } else {
-                        API.doAction(actionName, value);
+                    });
+                }
+
+                const customB = [];
+                const customE = [];
+                if (custom) {
+                    custom = custom.filter(el => el.title);
+                    for (let i = 0; i < custom.length; i++) {
+                        let el = {
+                            ifLocked: true,
+                            title: custom[i].title,
+                            cssClass: 'fa ' + custom[i].icon,
+                            color: Color.array2rgba(custom[i].color),
+                            onClick: function () {
+                                API.doAction(custom[i].action);
+                            }
+                        };
+                        if (custom[i].position === 'begin') {
+                            customB.push(el);
+                        } else {
+                            customE.push(el);
+                        }
                     }
                 }
-            }
-        },
 
-        sendAction: Util.deprecate(function sendAction(rel, value, event) {
-            return this.sendActionFromEvent(event, rel, value);
-        }, 'Use sendActionFromEvent instead.'),
+                return customB.concat(toolbar, customE);
+            },
 
-        allVariablesFor(event, rel, callback) {
-            const varsOut = this.module.vars_out();
-            if (!varsOut) {
-                return;
-            }
+            inDom: Util.noop,
 
-            for (let i = 0; i < varsOut.length; i++) {
-                if (
-                    varsOut[i].event == event &&
-                    (varsOut[i].rel == rel || !rel)
-                ) {
-                    callback(varsOut[i]);
+            setVarFromEvent(event, rel, relSource, jpath, callback) {
+                const varsOut = this.module.vars_out();
+                if (!varsOut) {
+                    return;
                 }
-            }
-        },
 
-        export: Util.noop,
+                let first = true;
+                for (let i = 0; i < varsOut.length; i++) {
+                    if (
+                        varsOut[i].event == event &&
+                        (varsOut[i].rel == rel || !rel) &&
+                        varsOut[i].name
+                    ) {
+                        if (first && callback) {
+                            first = false;
+                            callback.call(this);
+                        }
 
-        print() {
-            const $dom = this.module.getDomContent();
-            const $domCopy = $dom.clone();
-            const $canvas = $dom.find('canvas');
-            const $canvasCopy = $domCopy.find('canvas');
+                        varsOut[i].jpath = varsOut[i].jpath || []; // Need not be undefined
 
-            $canvas.each(function (index) {
-                const dataUrl = this.toDataURL();
-                const img = new Image();
-                img.src = dataUrl;
-                img.width = this.width;
-                img.height = this.height;
-                $($canvasCopy[index]).after(img);
-                $($canvasCopy[index]).remove();
-            });
-            return $domCopy[0];
-        },
+                        if (typeof varsOut[i].jpath == 'string') {
+                            varsOut[i].jpath = varsOut[i].jpath.split('.');
+                            varsOut[i].jpath.shift();
+                        }
 
-        configurationStructure: Util.noop,
-
-        configFunctions: {},
-
-        configAliases: {},
-
-        defaultReferences: {
-            _loadTime: {
-                label: 'Loading time'
+                        API.setVar(
+                            varsOut[i].name,
+                            this.module.getVariableFromRel(relSource),
+                            jpath.concat(varsOut[i].jpath),
+                            varsOut[i].filter
+                        );
+                    }
+                }
             },
-            _moduleUrl: {
-                label: 'Module URL'
+
+            createDataFromEvent(event, rel, data, callback) {
+                const varsOut = this.module.vars_out();
+                if (!varsOut) {
+                    return;
+                }
+
+                let first = true;
+                for (let i = 0; i < varsOut.length; i++) {
+                    if (
+                        varsOut[i].event == event &&
+                        (varsOut[i].rel == rel || !rel) &&
+                        varsOut[i].name
+                    ) {
+                        if (first && callback) {
+                            first = false;
+                            data = callback.call(this);
+                        }
+
+                        API.createDataJpath(
+                            varsOut[i].name,
+                            data,
+                            varsOut[i].jpath,
+                            varsOut[i].filter
+                        );
+                    }
+                }
             },
-            _varName: {
-                label: 'Variable name',
-                type: 'string'
-            }
-        },
 
-        references: {},
+            sendActionFromEvent(event, rel, value) {
+                const actionsOut = this.module.actions_out();
 
-        defaultEvents: {
-            _onLoaded: {
-                label: 'Module loaded',
-                refAction: ['_loadTime', '_moduleUrl']
+                if (!actionsOut) {
+                    return;
+                }
+
+                for (let i = actionsOut.length - 1; i >= 0; i--) {
+                    if (
+                        actionsOut[i].name &&
+                        actionsOut[i].rel === rel &&
+                        ((event && event === actionsOut[i].event) || !event)
+                    ) {
+                        const actionName = actionsOut[i].name;
+                        const jpath = actionsOut[i].jpath;
+
+                        if (value && jpath) {
+                            if (!value.getChild) {
+                                value = DataObject.check(value, true);
+                            }
+
+                            value.getChild(jpath).then(returned => {
+                                API.doAction(actionName, returned);
+                            });
+                        } else {
+                            API.doAction(actionName, value);
+                        }
+                    }
+                }
             },
-            _onVarUpdated: {
-                label: 'Variable updated',
-                refAction: ['_varName']
-            }
-        },
 
-        events: {},
+            sendAction: Util.deprecate(function sendAction(rel, value, event) {
+                return this.sendActionFromEvent(event, rel, value);
+            }, 'Use sendActionFromEvent instead.'),
 
-        variablesIn: [],
+            allVariablesFor(event, rel, callback) {
+                const varsOut = this.module.vars_out();
+                if (!varsOut) {
+                    return;
+                }
 
-        defaultActionsIn: {
-            _editPreferences: 'Edit preferences',
-            _print: 'Print module content'
-        },
-        actionsIn: {},
+                for (let i = 0; i < varsOut.length; i++) {
+                    if (
+                        varsOut[i].event == event &&
+                        (varsOut[i].rel == rel || !rel)
+                    ) {
+                        callback(varsOut[i]);
+                    }
+                }
+            },
 
-        resolveReady() {
-            this.module._resolveController();
-        },
+            export: Util.noop,
 
-        onBeforeRemove() {
-            return true;
-        },
+            print() {
+                const $dom = this.module.getDomContent();
+                const $domCopy = $dom.clone();
+                const $canvas = $dom.find('canvas');
+                const $canvasCopy = $domCopy.find('canvas');
 
-        onRemove: Util.noop
-    };
-});
+                $canvas.each(function (index) {
+                    const dataUrl = this.toDataURL();
+                    const img = new Image();
+                    img.src = dataUrl;
+                    img.width = this.width;
+                    img.height = this.height;
+                    $($canvasCopy[index]).after(img);
+                    $($canvasCopy[index]).remove();
+                });
+                return $domCopy[0];
+            },
+
+            configurationStructure: Util.noop,
+
+            configFunctions: {},
+
+            configAliases: {},
+
+            defaultReferences: {
+                _loadTime: {
+                    label: 'Loading time'
+                },
+                _moduleUrl: {
+                    label: 'Module URL'
+                },
+                _varName: {
+                    label: 'Variable name',
+                    type: 'string'
+                }
+            },
+
+            references: {},
+
+            defaultEvents: {
+                _onLoaded: {
+                    label: 'Module loaded',
+                    refAction: ['_loadTime', '_moduleUrl']
+                },
+                _onVarUpdated: {
+                    label: 'Variable updated',
+                    refAction: ['_varName']
+                }
+            },
+
+            events: {},
+
+            variablesIn: [],
+
+            defaultActionsIn: {
+                _editPreferences: 'Edit preferences',
+                _print: 'Print module content'
+            },
+            actionsIn: {},
+
+            resolveReady() {
+                this.module._resolveController();
+            },
+
+            onBeforeRemove() {
+                return true;
+            },
+
+            onRemove: Util.noop
+        };
+    }
+);
