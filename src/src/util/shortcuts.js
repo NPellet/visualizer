8a033c8daf3bedb4314976a4f133aea0ef83b049
Daniel Kostro <kostro.d@gmail.com>
2018-06-29 10:59:07 +0200

    add ctrl+m shortcut to search for layers / modules

--- src/src/util/shortcuts.js
+++ src/src/util/shortcuts.js
@@ -5,12 +5,11 @@ define(['jquery', 'src/util/versioning'], function ($, Versioning) {
         function (event) {
             // If Control or Command key is pressed and the S key is pressed
             // run save function. 83 is the key code for S.
-            if ((event.ctrlKey || event.metaKey) && !event.altKey && (event.which == 191 || event.which === 111)) {
+            if ((event.ctrlKey || event.metaKey) && !event.altKey && (event.which == 191 || event.which === 111 || event.which === 77)) {
                 event.preventDefault();
                 require(['src/util/searchBox'], function (searchModule) {
                     searchModule();
                 });
             }
-        }
-    );
+        });
 });
