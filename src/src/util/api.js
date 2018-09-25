9bfb6223f9bbf94ac24b755983211a5ac75fa8e0
Luc Patiny <luc@patiny.com>
2018-09-25 08:57:16 +0200

    fix eslint

--- src/src/util/api.js
+++ src/src/util/api.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* eslint-disable import/no-unresolved */
+
 /**
  * Main visualizer API
  * @module src/util/api
@@ -31,7 +33,9 @@ define([
   var loadingSVG = Util.getLoadingAnimation(64, 'slateblue');
   var loadingHtml = $('<div>', { id: 'ci-loading' })
     .append(loadingSVG)
-    .append($('<div>', { id: 'ci-loading-message', class: 'ci-loading-subtitle' }));
+    .append(
+      $('<div>', { id: 'ci-loading-message', class: 'ci-loading-subtitle' })
+    );
   var loading = {};
   var loadingNumber = 0;
 
@@ -40,7 +44,6 @@ define([
   }
 
   var exports = {
-
     getRepositoryData: function () {
       return this.repositoryData;
     },
@@ -71,11 +74,17 @@ define([
       }
 
       arguments[0] = arguments[0]._highlight;
-      this.repositoryHighlights.listen.apply(this.repositoryHighlights, arguments);
+      this.repositoryHighlights.listen.apply(
+        this.repositoryHighlights,
+        arguments
+      );
     },
 
     killHighlight: function () {
-      this.repositoryHighlights.kill.apply(this.repositoryHighlights, arguments);
+      this.repositoryHighlights.kill.apply(
+        this.repositoryHighlights,
+        arguments
+      );
     },
 
     highlightId: setHighlightId,
@@ -109,61 +118,70 @@ define([
     },
 
     /* Extra functions used in filter testsuite. Allows compatibility of filters */
-    dev_fctCalled: function (fct) {
-    },
-    dev_fctUncalled: function (fct) {
-    },
-    dev_assert: function (family, script, value) {
-    }
-
+    dev_fctCalled: function (fct) {},
+    dev_fctUncalled: function (fct) {},
+    dev_assert: function (family, script, value) {}
   };
 
-    /**
-     * Check if a variable is defined
-     * @param {string} varName - Name of the variable
-     * @return {boolean}
-     */
+  /**
+   * Check if a variable is defined
+   * @param {string} varName - Name of the variable
+   * @return {boolean}
+   */
   exports.existVariable = function existVariable(varName) {
     return Variables.exist(varName);
   };
   exports.existVar = exports.existVariable;
 
   /**
-     * Set a variable using a jpath
-     * @param {string} name - Name of the variable
-     * @param {Variable} [sourceVariable] - Source variable. If set, the new variable will be created relative to its jpath
-     * @param {string[]} jpath
-     * @param {string} [filter] - Url of the filter to use with this variable
-     * @return {Promise}
-     */
-  exports.setVariable = function setVariable(name, sourceVariable, jpath, filter) {
+   * Set a variable using a jpath
+   * @param {string} name - Name of the variable
+   * @param {Variable} [sourceVariable] - Source variable. If set, the new variable will be created relative to its jpath
+   * @param {string[]} jpath
+   * @param {string} [filter] - Url of the filter to use with this variable
+   * @return {Promise}
+   */
+  exports.setVariable = function setVariable(
+    name,
+    sourceVariable,
+    jpath,
+    filter
+  ) {
     if (Array.isArray(sourceVariable)) {
       filter = jpath;
       jpath = sourceVariable;
       sourceVariable = null;
     }
     jpath = jpath || [];
-    var jpathNewVar = (!sourceVariable) ? jpath : sourceVariable.getjPath().concat(jpath);
+    var jpathNewVar = !sourceVariable
+      ? jpath
+      : sourceVariable.getjPath().concat(jpath);
 
     return Variables.setVariable(name, jpathNewVar, false, filter);
   };
   exports.setVar = exports.setVariable;
 
   /**
-     * Create new data and set a variable to it
-     * @param {string} name - Name of the variable
-     * @param {*} data - Data to set
-     * @param {string} [filter] - Url of the filter to use with this variable
-     * @return {Promise}
-     */
+   * Create new data and set a variable to it
+   * @param {string} name - Name of the variable
+   * @param {*} data - Data to set
+   * @param {string} [filter] - Url of the filter to use with this variable
+   * @return {Promise}
+   */
   exports.createData = function createData(name, data, filter) {
     return exports.createDataJpath(name, data, [], filter);
   };
 
-  exports.createDataJpath = function createDataJpath(name, data, jpath, filter) {
+  exports.createDataJpath = function createDataJpath(
+    name,
+    data,
+    jpath,
+    filter
+  ) {
     data = Data.check(Data.resurrect(data), true);
     if (data && data.trace) {
-      return data.trace(jpath)
+      return data
+        .trace(jpath)
         .then((data) => Variables.setVariable(name, false, data, filter));
     } else {
       return Variables.setVariable(name, false, data, filter);
@@ -171,32 +189,31 @@ define([
   };
 
   /**
-     * Get a variable by name
-     * @param {string} name - Name of the variable
-     * @return {Variable}
-     */
+   * Get a variable by name
+   * @param {string} name - Name of the variable
+   * @return {Variable}
+   */
   exports.getVariable = function getVariable(name) {
     return Variables.getVariable(name);
   };
   exports.getVar = exports.getVariable;
 
   /**
-     * Get the DataObject associated to a variable
-     * @param {string} varName - Name of the variable
-     * @return {*} - DataObject or undefined
-     */
+   * Get the DataObject associated to a variable
+   * @param {string} varName - Name of the variable
+   * @return {*} - DataObject or undefined
+   */
   exports.getData = function getData(varName) {
     return exports.getVariable(varName).getData();
   };
 
   /**
-     * Change the state of a highlight
-     * @param {object|Array} element - Object with a _highlight property or array of highlight IDs
-     * @param {boolean} onOff
-     */
+   * Change the state of a highlight
+   * @param {object|Array} element - Object with a _highlight property or array of highlight IDs
+   * @param {boolean} onOff
+   */
   exports.setHighlight = function setHighlight(element, onOff) {
-    if (!element)
-      return;
+    if (!element) return;
 
     if (Array.isArray(element)) {
       element = { _highlight: element };
@@ -211,10 +228,10 @@ define([
   exports.highlight = exports.setHighlight;
 
   /**
-     * Set a loading message or change the value of an existing message
-     * @param {string} id - ID of the message
-     * @param {string} [message] - Message content (default: value of the ID)
-     */
+   * Set a loading message or change the value of an existing message
+   * @param {string} id - ID of the message
+   * @param {string} [message] - Message content (default: value of the ID)
+   */
   exports.loading = function setLoading(id, message) {
     if (!message) {
       message = id;
@@ -235,9 +252,9 @@ define([
   };
 
   /**
-     * Remove a loading message
-     * @param {string} id - ID of the message
-     */
+   * Remove a loading message
+   * @param {string} id - ID of the message
+   */
   exports.stopLoading = function stopLoading(id) {
     if (loading[id]) {
       loadingNumber--;
@@ -251,10 +268,10 @@ define([
   };
 
   /**
-     * Send an action to all modules and global action scripts
-     * @param {string} name - Action name
-     * @param {*} [value] - Action value
-     */
+   * Send an action to all modules and global action scripts
+   * @param {string} name - Action name
+   * @param {*} [value] - Action value
+   */
   exports.doAction = function doAction(name, value) {
     if (Data.isSpecialObject(value)) {
       value = value.get();
@@ -264,21 +281,21 @@ define([
   };
 
   /**
-     * @deprecated
-     * Execute a global visualizer action. This is deprecated. Use API.doAction instead.
-     * @param {string} name - Action name
-     * @param {*} value - Action value
-     */
+   * @deprecated
+   * Execute a global visualizer action. This is deprecated. Use API.doAction instead.
+   * @param {string} name - Action name
+   * @param {*} value - Action value
+   */
   exports.executeAction = Util.deprecate(function executeAction(name, value) {
     ActionManager.execute(name, value);
   }, 'API.doAction is the recommended method.');
 
   /**
-     * Cache a value in memory or retrieve it. The value can be retrieved anywhere API is available
-     * @param {string} name - Name of the cached value
-     * @param {*} [value] - New value to set
-     * @return {*} The cached value or undefined if used as a setter
-     */
+   * Cache a value in memory or retrieve it. The value can be retrieved anywhere API is available
+   * @param {string} name - Name of the cached value
+   * @param {*} [value] - New value to set
+   * @return {*} The cached value or undefined if used as a setter
+   */
   exports.cache = function cacheHandler(name, value) {
     if (arguments.length === 1) {
       return Cache.get(name);
