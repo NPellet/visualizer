716de6bcf16bb2f0c170720c23978eed70d2c3f7
Luc Patiny <luc@patiny.com>
2019-07-03 14:07:49 +0200

    remove console log for add / remove annotations

--- src/modules/types/science/spectra/spectra_displayer/view.js
+++ src/modules/types/science/spectra/spectra_displayer/view.js
@@ -746,7 +746,6 @@ define([
       },
 
       annotations(varName) {
-        console.log('remove annotation: ', varName);
         this.removeAnnotations(varName);
       }
     },
@@ -1040,7 +1039,6 @@ define([
       },
 
       annotations(value, varName) {
-        console.log('add annotation: ', varName);
         this.annotations[varName] = this.annotations[varName] || [];
         const annotations = value.get();
         for (let i = 0; i < annotations.length; i++) {
