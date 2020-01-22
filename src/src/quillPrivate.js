4cf76ea960374bbe03a4d85dc087b991cf933db8
Luc Patiny <luc@patiny.com>
2020-01-22 15:22:21 +0100

    feat: use bluebird and not bluebird.core to have allSettled

--- src/src/quillPrivate.js
+++ src/src/quillPrivate.js
@@ -1,9 +1,6 @@
 'use strict';
 
-define([
-  'katex',
-  'quill'
-], function (katex, Quill, ImageResize) {
+define(['katex', 'quill'], function (katex, Quill, ImageResize) {
   window.katex = katex; // Needed for quill formula to work :(
   window.Quill = Quill;
   return Quill;
