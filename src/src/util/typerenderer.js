3bd432c8f6d96cff4a48268356fd298f2e3ca539
Luc Patiny <luc@patiny.com>
2020-09-04 12:19:13 +0200

    chore: fix eslint

--- src/src/util/typerenderer.js
+++ src/src/util/typerenderer.js
@@ -9,31 +9,31 @@ define([
   'sprintf',
   './util',
   './typerenderer/chart',
-], function(require, $, _, moment, numeral, sprintf, Util, chartRenderer) {
+], function (require, $, _, moment, numeral, sprintf, Util, chartRenderer) {
   const asyncRequire = Util.require;
 
   const functions = {};
 
   functions.latex = {};
-  functions.latex.init = async function() {
+  functions.latex.init = async function () {
     const css = Util.loadCss('node_modules/katex/dist/katex.min.css');
     functions.latex.katex = await asyncRequire('katex');
     await css;
   };
 
-  functions.latex.toscreen = function($element, val, rootVal, options) {
+  functions.latex.toscreen = function ($element, val, rootVal, options) {
     $element.empty();
     functions.latex.katex.render(String(val), $element[0], options);
   };
 
   let countryData;
   functions.country = {};
-  functions.country.init = async function() {
+  functions.country.init = async function () {
     const css = Util.loadCss('components/flag-icon-css/css/flag-icon.min.css');
     countryData = await asyncRequire('countryData');
     await css;
   };
-  functions.country.toscreen = function($element, val, rootVal, options) {
+  functions.country.toscreen = function ($element, val, rootVal, options) {
     val = String(val);
     var country;
     if (val.length === 2) {
@@ -58,10 +58,10 @@ define([
   };
 
   functions.qrcode = {};
-  functions.qrcode.init = async function() {
+  functions.qrcode.init = async function () {
     await asyncRequire('components/jquery-qrcode/jquery.qrcode.min');
   };
-  functions.qrcode.toscreen = function($element, val, rootVal, options) {
+  functions.qrcode.toscreen = function ($element, val, rootVal, options) {
     options = Object.assign(
       {
         width: 128,
@@ -75,11 +75,11 @@ define([
   };
 
   functions.barcode = {};
-  functions.barcode.init = async function() {
+  functions.barcode.init = async function () {
     await asyncRequire('jsbarcode');
   };
 
-  functions.barcode.toscreen = function($element, val, rootVal, options) {
+  functions.barcode.toscreen = function ($element, val, rootVal, options) {
     var defaultOptions = {
       format: 'CODE128',
     };
@@ -95,10 +95,10 @@ define([
   };
 
   functions.sparkline = {};
-  functions.sparkline.init = async function() {
+  functions.sparkline.init = async function () {
     await asyncRequire('sparkline');
   };
-  functions.sparkline.toscreen = function($el, val, rootval, options) {
+  functions.sparkline.toscreen = function ($el, val, rootval, options) {
     var defaultOptions = {
       width: options.type === 'discrete' ? 'auto' : '100%',
       height: '100%',
@@ -108,7 +108,7 @@ define([
   };
 
   functions.string = {};
-  functions.string.toscreen = function($element, val, rootVal, options) {
+  functions.string.toscreen = function ($element, val, rootVal, options) {
     val = String(val);
     val = val.replace(/</g, '&lt;').replace(/>/g, '&gt;');
 
@@ -129,12 +129,12 @@ define([
   };
 
   functions.html = {};
-  functions.html.toscreen = function($element, val) {
+  functions.html.toscreen = function ($element, val) {
     $element.html(String(val));
   };
 
   functions.date = {};
-  functions.date.toscreen = function($element, val) {
+  functions.date.toscreen = function ($element, val) {
     try {
       var d = new Date(val);
       $element.html(d.toLocaleString());
@@ -144,7 +144,7 @@ define([
   };
 
   functions.color = {};
-  functions.color.toscreen = function($element, val) {
+  functions.color.toscreen = function ($element, val) {
     var result = `${'<div style="background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAIAAADZF8uwAAAAGUlEQVQYV2M4gwH+YwCGIasIUwhT25BVBADtzYNYrHvv4gAAAABJRU5ErkJggg==); width:100%; height:100%">' +
       '<div style="background-color: '}${val}; width: 100%; height:100%; min-height: 1px; padding:0; margin:0"></div></div>`;
     $element.html(result);
@@ -191,18 +191,18 @@ define([
   }
 
   functions.number = {};
-  functions.number.toscreen = function($element, val, rootVal, options) {
+  functions.number.toscreen = function ($element, val, rootVal, options) {
     const number = formatNumber(val, options);
     $element.html(number);
   };
 
   functions.jpath = {};
-  functions.jpath.toscreen = function($element, val, rootVal, options) {
+  functions.jpath.toscreen = function ($element, val, rootVal, options) {
     $element.html(val.join('.'));
   };
 
   functions.unit = {};
-  functions.unit.toscreen = async function($element, val, rootVal, options) {
+  functions.unit.toscreen = async function ($element, val, rootVal, options) {
     if (!val) return;
     let displayValue;
     if (typeof val === 'number') {
@@ -237,7 +237,7 @@ define([
   };
 
   functions.picture = {};
-  functions.picture.toscreen = function(element, val, rootVal, options) {
+  functions.picture.toscreen = function (element, val, rootVal, options) {
     var $img = $('<img>');
     $img.attr({
       src: val,
@@ -260,7 +260,7 @@ define([
   functions.image = functions.picture;
 
   functions.svg = {};
-  functions.svg.toscreen = function($element, val) {
+  functions.svg.toscreen = function ($element, val) {
     var dom = $(String(val));
     let width = dom.attr('width');
     let height = dom.attr('height');
@@ -276,7 +276,7 @@ define([
   };
 
   functions.ghs = {};
-  functions.ghs.toscreen = function($element, val) {
+  functions.ghs.toscreen = function ($element, val) {
     var height = $element.height();
 
     var ghs = {};
@@ -305,7 +305,7 @@ define([
   };
 
   functions.doi = {};
-  functions.doi.toscreen = function($element, value) {
+  functions.doi.toscreen = function ($element, value) {
     return $element.html(
       value.replace(
         /^(.*)$/,
@@ -369,21 +369,21 @@ define([
   }
 
   functions.jme = {};
-  functions.jme.toscreen = async function($element, jme, jmeRoot, options) {
+  functions.jme.toscreen = async function ($element, jme, jmeRoot, options) {
     const Converter = await asyncRequire('lib/chemistry/jme-converter');
     const converted = Converter.toMolfile(String(jme));
     return functions.mol2d.toscreen($element, converted, jmeRoot, options);
   };
 
   functions.smiles = {};
-  functions.smiles.toscreen = async function($element, smi, smiRoot, options) {
+  functions.smiles.toscreen = async function ($element, smi, smiRoot, options) {
     const OCL = await asyncRequire(oclUrl);
     const mol = OCL.Molecule.fromSmiles(String(smi));
     return renderOpenChemLibStructure(true, $element, mol, false, options);
   };
 
   functions.rxn = {};
-  functions.rxn.toscreen = async function($element, val, root, options = {}) {
+  functions.rxn.toscreen = async function ($element, val, root, options = {}) {
     const { maxWidth = 300, maxHeight = 300 } = options;
     const OCL = await asyncRequire(oclUrl);
     const { RxnRenderer } = await asyncRequire('RxnRenderer');
@@ -393,7 +393,7 @@ define([
   };
 
   functions.reaction = {};
-  functions.reaction.toscreen = async function(
+  functions.reaction.toscreen = async function (
     $element,
     val,
     root,
@@ -408,7 +408,7 @@ define([
   };
 
   functions.oclid = {};
-  functions.oclid.toscreen = async function($element, val, root, options) {
+  functions.oclid.toscreen = async function ($element, val, root, options) {
     const OCL = await asyncRequire(oclUrl);
     if (!root.coordinates) {
       const mol = OCL.Molecule.fromIDCode(String(val), true);
@@ -430,7 +430,7 @@ define([
   functions.actelionid = functions.oclid;
 
   functions.mol2d = {};
-  functions.mol2d.toscreen = async function(
+  functions.mol2d.toscreen = async function (
     $element,
     molfile,
     molfileRoot,
@@ -449,7 +449,7 @@ define([
   functions.molfile2d = functions.mol2d;
 
   functions.elecconfig = {};
-  functions.elecconfig.toscreen = function($element, value) {
+  functions.elecconfig.toscreen = function ($element, value) {
     if (value) {
       $element.html(value.replace(/([a-z])([0-9]+)/g, '$1<sup>$2</sup>'));
     } else {
@@ -458,7 +458,7 @@ define([
   };
 
   functions.mf = {};
-  functions.mf.toscreen = async function($element, value) {
+  functions.mf.toscreen = async function ($element, value) {
     if (value) {
       const { parseToHtml } = await asyncRequire('MFParser');
       try {
@@ -487,12 +487,12 @@ define([
       height: Math.max(250, element.height() * 0.99),
       quality: 'medium',
     });
-    viewer.addListener('viewerReady', function() {
+    viewer.addListener('viewerReady', function () {
       options.mode = viewer[options.mode] ? options.mode : 'cartoon';
       var id = Util.getNextUniqueId();
       if (type === 'pdb') {
         viewer.clear();
-        mol.forEach(function(structure) {
+        mol.forEach(function (structure) {
           if (options.mode === 'cartoon') {
             var ligand = structure.select({
               rnames: ['RVP', 'SAH'],
@@ -523,7 +523,7 @@ define([
   functions.molfile3d = functions.mol3d;
 
   functions.downloadlink = {};
-  functions.downloadlink.toscreen = function($element, value, root, options) {
+  functions.downloadlink.toscreen = function ($element, value, root, options) {
     var title = options && options.title;
     title = title || 'Download resource';
 
@@ -535,7 +535,7 @@ define([
   };
 
   functions.openlink = {};
-  functions.openlink.toscreen = function($element, value) {
+  functions.openlink.toscreen = function ($element, value) {
     $element.html(
       value.replace(
         /^(.*)$/,
@@ -545,7 +545,7 @@ define([
   };
 
   functions.boolean = {};
-  functions.boolean.toscreen = function($element, value) {
+  functions.boolean.toscreen = function ($element, value) {
     if (value instanceof DataBoolean) {
       value = value.get();
     }
@@ -554,10 +554,10 @@ define([
   };
 
   functions.gradient = {};
-  functions.gradient.init = async function() {
+  functions.gradient.init = async function () {
     functions.gradient.colorbar = await asyncRequire('src/util/colorbar');
   };
-  functions.gradient.toscreen = function($element, value, root, options) {
+  functions.gradient.toscreen = function ($element, value, root, options) {
     var defaultColorBar = {
       domain: [0, 1],
       stopType: 'values',
@@ -573,7 +573,7 @@ define([
   };
 
   functions.colorbar = {};
-  functions.colorbar.toscreen = function($element, value) {
+  functions.colorbar.toscreen = function ($element, value) {
     var div = $('<div></div>');
     var gradient = 'linear-gradient(to right';
 
@@ -604,7 +604,7 @@ define([
   };
 
   functions.indicator = {};
-  functions.indicator.init = function() {
+  functions.indicator.init = function () {
     var tooltip = $('<div class="ci-tooltip"></div>')
       .css({
         display: 'none',
@@ -614,8 +614,8 @@ define([
     var current;
 
     var $modulesGrid = $('#modules-grid');
-    $modulesGrid.on('mouseenter', '[data-tooltip]', function(e) {
-      current = setTimeout(function() {
+    $modulesGrid.on('mouseenter', '[data-tooltip]', function (e) {
+      current = setTimeout(function () {
         var target = $(e.target);
         var offset = target.offset();
         tooltip
@@ -631,7 +631,7 @@ define([
       }, 500);
     });
 
-    $modulesGrid.on('mouseleave', '[data-tooltip]', function(event) {
+    $modulesGrid.on('mouseleave', '[data-tooltip]', function (event) {
       clearTimeout(current);
       tooltip.css({
         opacity: 0,
@@ -639,7 +639,7 @@ define([
       });
     });
   };
-  functions.indicator.toscreen = async function($element, value) {
+  functions.indicator.toscreen = async function ($element, value) {
     const Color = await asyncRequire('src/util/color');
     if (!Array.isArray(value)) {
       return;
@@ -654,7 +654,7 @@ define([
     value = _.cloneDeep(value);
 
     if (!isNaN(value[0])) {
-      value = value.map(function(value) {
+      value = value.map(function (value) {
         return { size: value };
       });
     }
@@ -694,7 +694,7 @@ define([
   };
 
   functions.regexp = {};
-  functions.regexp.toscreen = async function($element, val) {
+  functions.regexp.toscreen = async function ($element, val) {
     const value = String(val);
     const Parser = await asyncRequire('lib/regexper/regexper');
     const div = $('<div>').appendTo($element);
@@ -705,10 +705,10 @@ define([
   functions.regex = functions.regexp;
 
   functions.object = {};
-  functions.object.init = async function() {
+  functions.object.init = async function () {
     functions.object.twig = await asyncRequire('lib/twigjs/twig');
   };
-  functions.object.toscreen = function($element, value, root, options) {
+  functions.object.toscreen = function ($element, value, root, options) {
     if (options.twig) {
       const template = functions.object.twig.twig({ data: options.twig });
       const render = template.renderAsync(JSON.parse(JSON.stringify(value)));
@@ -786,7 +786,7 @@ define([
 
       var callback = () => {
         if (jpath) {
-          return object.getChild(jpath).then(function(child) {
+          return object.getChild(jpath).then(function (child) {
             return _render($element, child, options);
           });
         } else {
