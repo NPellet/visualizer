ad4a5113a5ef900c9e0e421d8027ef99d71f699a
Luc Patiny <luc@patiny.com>
2021-01-23 07:07:36 +0100

    fix: jcamp renderer axis labels

--- src/src/util/typerenderer/jcamp.js
+++ src/src/util/typerenderer/jcamp.js
@@ -41,16 +41,29 @@ define(['../util'], function (Util) {
 
     el.empty();
     let chart = JSON.parse(JSON.stringify(defaultChart));
-    let options = JSON.parse(JSON.stringify(_options));
-    if (typeof options === 'object') {
-      _.merge(chart, options.chart);
-    }
     let parsed = JcampConverter.convert(String(jcamp));
-    let spectrum = parsed.flatten[0].spectra[0];
-    chart.series[0].data = spectrum.data;
-    const graph = Graph.fromJSON(chart, el.get(0), () => { });
-    graph.resize(Math.max(el.width() - 15, 20), Math.max(el.height() - 15, 20));
-    graph.draw();
+
+    if (parsed.flatten[0] && parsed.flatten[0].spectra && parsed.flatten[0].spectra[0]) {
+      let spectrum = parsed.flatten[0].spectra[0];
+      let options = JSON.parse(JSON.stringify(_options));
+      _.merge(chart, {
+        axes: {
+          x: {
+            label: spectrum.xUnits,
+          },
+          y: {
+            label: spectrum.yUnits,
+          }
+        }
+      }, options.chart);
+
+
+      chart.series[0].data = spectrum.data;
+      const graph = Graph.fromJSON(chart, el.get(0), () => { });
+      graph.resize(Math.max(el.width() - 15, 20), Math.max(el.height() - 15, 20));
+      graph.draw();
+    }
+
 
     return el;
   }
