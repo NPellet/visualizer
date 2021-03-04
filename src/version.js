297788c7c42e153f8246ade25073c48fba2daf95
Luc Patiny <luc@patiny.com>
2021-03-04 07:55:34 +0100

    Working on v2.127.1-0

--- src/version.js
+++ src/version.js
@@ -4,9 +4,9 @@ define(['lib/semver/semver'], function (semver) {
   /* DO NOT MODIFY THE FOLLOWING LINES MANUALLY */
   const MAJOR = 2;
   const MINOR = 127;
-  const PATCH = 0;
-  const PRERELEASE = false;
-  const IS_RELEASE = true;
+  const PATCH = 1;
+  const PRERELEASE = 0;
+  const IS_RELEASE = false;
   const BUILD_TIME = null;
   const INCLUDED_MODULE_CSS = [];
   /* END */
