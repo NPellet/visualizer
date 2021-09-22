01022846701845029ba7a29d425eca5670d5f7ca
Luc Patiny <luc@patiny.com>
2021-09-22 21:11:55 +0200

    fix: onBondClicked event in JSME

--- src/modules/types/science/chemistry/jsme/controller.js
+++ src/modules/types/science/chemistry/jsme/controller.js
@@ -1,27 +1,34 @@
 'use strict';
 
-define(['modules/default/defaultcontroller', 'src/util/ui'], function (Default, ui) {
-  function Controller() {
-  }
+define(['modules/default/defaultcontroller', 'src/util/ui'], function(
+  Default,
+  ui,
+) {
+  function Controller() {}
 
   $.extend(true, Controller.prototype, Default);
 
-  Controller.prototype.getToolbar = function () {
+  Controller.prototype.getToolbar = function() {
     var base = Default.getToolbar.call(this);
     base.unshift({
-      onClick: function () {
+      onClick: function() {
         var w = $(window).width();
         var h = $(window).height();
-        var url = require.toUrl('modules/types/science/chemistry/jsme/help/index.html');
-        ui.dialog(`<iframe src=${url} width="100%", height="100%" frameBorder="0"></iframe>`, {
-          width: Math.min(w - 40, 800),
-          height: h - 70,
-          title: 'JSME Help'
-        });
+        var url = require.toUrl(
+          'modules/types/science/chemistry/jsme/help/index.html',
+        );
+        ui.dialog(
+          `<iframe src=${url} width="100%", height="100%" frameBorder="0"></iframe>`,
+          {
+            width: Math.min(w - 40, 800),
+            height: h - 70,
+            title: 'JSME Help',
+          },
+        );
       },
       title: 'Help',
       cssClass: 'fa fa-question',
-      ifLocked: true
+      ifLocked: true,
     });
     return base;
   };
@@ -32,78 +39,77 @@ define(['modules/default/defaultcontroller', 'src/util/ui'], function (Default, 
     author: 'Norman Pellet, Luc Patiny',
     date: '24.12.2013',
     license: 'MIT',
-    cssClass: 'jsme'
+    cssClass: 'jsme',
   };
 
   Controller.prototype.references = {
     mol: {
       label: 'Molfile V2 2D',
-      type: ['mol2d', 'molfile2d', 'string']
+      type: ['mol2d', 'molfile2d', 'string'],
     },
     smiles: {
       label: 'SMILES',
-      type: ['smiles', 'string']
+      type: ['smiles', 'string'],
     },
     jme: {
       label: 'JME file format',
-      type: ['jme', 'string']
+      type: ['jme', 'string'],
     },
     svg: {
       label: 'SVG containing the picture',
-      type: ['svg', 'string']
+      type: ['svg', 'string'],
     },
     molV3: {
       label: 'Molfile V3 2D',
-      type: ['mol2d', 'molfile2d', 'string']
+      type: ['mol2d', 'molfile2d', 'string'],
     },
     atom: {
-      label: 'Atom'
+      label: 'Atom',
     },
     bond: {
-      label: 'Bond'
-    }
+      label: 'Bond',
+    },
   };
 
   Controller.prototype.events = {
     onStructureChange: {
       label: 'Molecular structure has changed',
-      refVariable: ['mol', 'smiles', 'jme', 'molV3', 'svg']
+      refVariable: ['mol', 'smiles', 'jme', 'molV3', 'svg'],
     },
     onAtomClicked: {
       label: 'An atom was clicked',
-      refAction: ['atom']
+      refAction: ['atom'],
     },
     onBondClicked: {
       label: 'A bond was clicked',
-      refAction: ['bond']
+      refAction: ['bond'],
     },
     onAtomHover: {
       label: 'An atom was hovered',
-      refAction: ['atom']
+      refAction: ['atom'],
     },
     onBondHover: {
       label: 'A bond was hovered',
-      refAction: ['bond']
+      refAction: ['bond'],
     },
     onMolfileLoaded: {
       label: 'A non empty molfile was loaded',
-      refAction: ['mol']
-    }
+      refAction: ['mol'],
+    },
   };
 
   Controller.prototype.actionsIn = $.extend({}, Default.actionsIn, {
-    setOptions: 'Set JSME options'
+    setOptions: 'Set JSME options',
   });
-
 
   Controller.prototype.variablesIn = ['mol', 'jme', 'smiles'];
 
-  Controller.prototype.configurationStructure = function () {
+  Controller.prototype.configurationStructure = function() {
     return {
       groups: {
         group: {
           options: {
-            type: 'list'
+            type: 'list',
           },
           fields: {
             prefs: {
@@ -115,35 +121,44 @@ define(['modules/default/defaultcontroller', 'src/util/ui'], function (Default, 
                 rbutton: 'Show R button',
                 nohydrogens: 'Hide hydrogens',
                 query: 'Enable query features',
-                autoez: 'Automatic generation of SMILES with E,Z stereochemistry',
-                nocanonize: 'Prevent canonicalization and detection of aromaticity',
+                autoez:
+                  'Automatic generation of SMILES with E,Z stereochemistry',
+                nocanonize:
+                  'Prevent canonicalization and detection of aromaticity',
                 nostereo: 'No stereochemistry in SMILES',
                 reaction: 'Enable reaction input',
-                nomultipart: 'Don\'t allow multipart structures',
+                nomultipart: "Don't allow multipart structures",
                 polarnitro: "Don't convert automatically nitro to unpolar form",
                 number: 'Allow to number atoms',
                 depict: 'Only display structure (no editing)',
                 nopaste: 'Remove the paste menu',
-                border: 'With depict option, display the border around the molecule',
-                star: 'Display star button allowing highlight of atoms (placed in the smiles)',
+                border:
+                  'With depict option, display the border around the molecule',
+                star:
+                  'Display star button allowing highlight of atoms (placed in the smiles)',
                 depictaction: 'Allows to specify an action in depict mode',
                 oldlook: 'Use the old look',
                 atommovebutton: 'Add button to move atoms',
-                removehs: 'Remove hydrogens when reading a molecule with explicit hydrogens',
-                removehsc: 'Remove hydrogens bounded to C when reading a molecule with explicit hydrogens',
+                removehs:
+                  'Remove hydrogens when reading a molecule with explicit hydrogens',
+                removehsc:
+                  'Remove hydrogens bounded to C when reading a molecule with explicit hydrogens',
                 nozoom: 'Prevent zoom with scroll wheel',
                 toggle: 'Allow to switch from edition to depict mode',
-                noshowdraganddropsymbolindepictmode: 'Hide the drag and drop symbol in depict mode',
-                addnewpart: 'when reading or pasting a new molecule, add it to the existing molecules',
-                useopenchemlib: 'use OpenChemLib library for SMILES conversion, 2D coordinates computation and SVG export',
+                noshowdraganddropsymbolindepictmode:
+                  'Hide the drag and drop symbol in depict mode',
+                addnewpart:
+                  'when reading or pasting a new molecule, add it to the existing molecules',
+                useopenchemlib:
+                  'use OpenChemLib library for SMILES conversion, 2D coordinates computation and SVG export',
                 exportSVG: 'enable / disable the menu item for SVG export',
-                nocontextmenu: 'Disable context menu'
-              }
+                nocontextmenu: 'Disable context menu',
+              },
             },
             defaultaction: {
               type: 'combo',
               title: 'Default action',
-              options: [{ title: 'Mark action', key: '105' }]
+              options: [{ title: 'Mark action', key: '105' }],
             },
             labelsize: {
               type: 'combo',
@@ -160,9 +175,9 @@ define(['modules/default/defaultcontroller', 'src/util/ui'], function (Default, 
                 { title: '14pt', key: '14' },
                 { title: '16pt', key: '16' },
                 { title: '18pt', key: '18' },
-                { title: '24pt', key: '24' }
+                { title: '24pt', key: '24' },
               ],
-              default: 14
+              default: 14,
             },
             bondwidth: {
               type: 'combo',
@@ -170,9 +185,9 @@ define(['modules/default/defaultcontroller', 'src/util/ui'], function (Default, 
               options: [
                 { title: '1px', key: '1' },
                 { title: '1.5px', key: '1.5' },
-                { title: '2px', key: '2' }
+                { title: '2px', key: '2' },
               ],
-              default: 1
+              default: 1,
             },
             highlightColor: {
               type: 'combo',
@@ -183,35 +198,37 @@ define(['modules/default/defaultcontroller', 'src/util/ui'], function (Default, 
                 { title: 'yellow', key: '3' },
                 { title: 'cyan', key: '4' },
                 { title: 'blue', key: '5' },
-                { title: 'purple', key: '6' }
+                { title: 'purple', key: '6' },
               ],
-              default: '3'
+              default: '3',
             },
             outputResult: {
               type: 'checkbox',
               title: 'Modify Input Variable',
               options: { yes: 'Yes' },
-              default: []
-            }
-          }
-        }
-      }
+              default: [],
+            },
+          },
+        },
+      },
     };
   };
 
   /*
-     * We receive an event from JSME
-     */
-  Controller.prototype.onChange = function (message) {
+   * We receive an event from JSME
+   */
+  Controller.prototype.onChange = function(message) {
     var action = message.action;
     var currentValue = this.module.view._currentValue;
     // check Github History when drag / drop and paste will be another action name
     if (
       action != null &&
-            ((action !== 'readRXNFile' && action !== 'readMolFile') || message.origin === 'GUI' || message.origin === 'PASTE') &&
-            action !== 'reset' &&
-            currentValue &&
-            this.module.getConfigurationCheckbox('outputResult', 'yes')
+      ((action !== 'readRXNFile' && action !== 'readMolFile') ||
+        message.origin === 'GUI' ||
+        message.origin === 'PASTE') &&
+      action !== 'reset' &&
+      currentValue &&
+      this.module.getConfigurationCheckbox('outputResult', 'yes')
     ) {
       if (this.module.view._currentType === 'mol') {
         currentValue.setValue(message.mol, true);
@@ -234,24 +251,28 @@ define(['modules/default/defaultcontroller', 'src/util/ui'], function (Default, 
     this.createDataFromEvent('onStructureChange', 'smiles', message.smiles);
     this.createDataFromEvent('onStructureChange', 'svg', {
       type: 'svg',
-      value: message.svg
+      value: message.svg,
     });
     this.createDataFromEvent('onStructureChange', 'mol', {
       type: 'mol2d',
-      value: message.mol
+      value: message.mol,
     });
     this.createDataFromEvent('onStructureChange', 'molV3', {
       type: 'mol2d',
-      value: message.molV3
+      value: message.molV3,
     });
     this.createDataFromEvent('onStructureChange', 'jme', {
       type: 'jme',
-      value: message.jme
+      value: message.jme,
     });
   };
 
-  Controller.prototype.getHighlights = function (atom) {
-    if (!this.module.view._currentValue || !this.module.view._currentValue._atoms) return;
+  Controller.prototype.getHighlights = function(atom) {
+    if (
+      !this.module.view._currentValue ||
+      !this.module.view._currentValue._atoms
+    )
+      return;
     var atoms = this.module.view._currentValue._atoms;
     var highlights = [];
     for (var key of Object.keys(atoms)) {
@@ -262,25 +283,25 @@ define(['modules/default/defaultcontroller', 'src/util/ui'], function (Default, 
     return highlights;
   };
 
-  Controller.prototype.onAtomClick = function (atom) {
+  Controller.prototype.onAtomClick = function(atom) {
     atom.highlights = this.getHighlights(atom.atom - 1);
     this.sendActionFromEvent('onAtomClicked', 'atom', atom);
   };
 
-  Controller.prototype.onAtomHover = function (atom) {
+  Controller.prototype.onAtomHover = function(atom) {
     atom.highlights = this.getHighlights(atom.atom - 1);
     this.sendActionFromEvent('onAtomHover', 'atom', atom);
   };
 
-  Controller.prototype.onBondClick = function (bond) {
-    this.sendActionFromEvent('onBondClick', 'bond', bond);
+  Controller.prototype.onBondClick = function(bond) {
+    this.sendActionFromEvent('onBondClicked', 'bond', bond);
   };
 
-  Controller.prototype.onBondHover = function (bond) {
+  Controller.prototype.onBondHover = function(bond) {
     this.sendActionFromEvent('onBondHover', 'bond', bond);
   };
 
-  Controller.prototype.onMolfileLoaded = function (molfile) {
+  Controller.prototype.onMolfileLoaded = function(molfile) {
     this.sendActionFromEvent('onMolfileLoaded', 'mol', molfile);
   };
 
@@ -290,10 +311,10 @@ define(['modules/default/defaultcontroller', 'src/util/ui'], function (Default, 
     bondwidth: ['groups', 'group', 0, 'bondwidth', 0],
     defaultaction: ['groups', 'group', 0, 'defaultaction', 0],
     highlightColor: ['groups', 'group', 0, 'highlightColor', 0],
-    outputResult: ['groups', 'group', 0, 'outputResult', 0]
+    outputResult: ['groups', 'group', 0, 'outputResult', 0],
   };
 
-  Controller.prototype.onRemove = function () {
+  Controller.prototype.onRemove = function() {
     this.module.view.remove(this.module.getId());
   };
 
--- src/modules/types/science/chemistry/jsme/view.js
+++ src/modules/types/science/chemistry/jsme/view.js
@@ -6,13 +6,13 @@ define([
   'src/util/api',
   'src/util/ui',
   'src/util/debug',
-  'openchemlib/openchemlib-core'
-], function (require, Default, API, ui, Debug, OCL) {
+  'openchemlib/openchemlib-core',
+], function(require, Default, API, ui, Debug, OCL) {
   function View() {}
 
   var views = {};
 
-  window.addEventListener('message', function (event) {
+  window.addEventListener('message', function(event) {
     try {
       var message = JSON.parse(event.data);
     } catch (e) {
@@ -56,7 +56,7 @@ define([
   });
 
   $.extend(true, View.prototype, Default, {
-    init: function () {
+    init: function() {
       var that = this;
 
       var id = this.module.getId();
@@ -64,79 +64,77 @@ define([
 
       this.dom = ui.getSafeElement('iframe').attr({
         allowfullscreen: true,
-        src: require.toUrl('./jsme.html')
+        src: require.toUrl('./jsme.html'),
       });
 
       this.module.getDomContent().html(this.dom);
       this.module.getDomContent().css('overflow', 'hidden');
 
-      this.dom.bind('load', function () {
+      this.dom.bind('load', function() {
         that.postMessage('init', {
           prefs: that.getPrefs(),
           highlightColor: that.getHighlightColor(),
           bondwidth: that.module.getConfiguration('bondwidth'),
           labelsize: that.module.getConfiguration('labelsize'),
-          defaultaction: that.module.getConfiguration(
-            'defaultaction'
-          ),
-          id: id
+          defaultaction: that.module.getConfiguration('defaultaction'),
+          id: id,
         });
       });
     },
 
-    setJSMEOptions: function (options) {
+    setJSMEOptions: function(options) {
       options = Object.assign({}, options, {
         prefs: options.prefs
           ? options.prefs.map((d) => String(d)).join()
-          : undefined
+          : undefined,
       });
       this.postMessage('setOptions', options);
     },
 
-    getPrefs: function () {
+    getPrefs: function() {
       return this.module.getConfiguration('prefs').join();
     },
 
-    getHighlightColor: function () {
+    getHighlightColor: function() {
       return this.module.getConfiguration('highlightColor', '3');
     },
 
-    onResize: function () {
+    onResize: function() {
       this.dom.attr('width', this.width);
       this.dom.attr('height', this.height);
 
       this.postMessage('setSize', {
         width: this.width,
-        height: this.height
+        height: this.height,
       });
 
       this.refresh();
     },
 
-    onProgress: function () {
+    onProgress: function() {
       this.dom.html('Progress. Please wait...');
     },
 
     blank: {
-      mol: function () {
+      mol: function() {
         this._currentValue = null;
         this._currentType = null;
         this.postMessage('clear', '*');
       },
-      jme: function () {
+      jme: function() {
         this._currentValue = null;
         this._currentType = null;
         this.postMessage('clear', '*');
       },
-      smiles: function () {
+      smiles: function() {
         this._currentValue = null;
         this._currentType = null;
         this.postMessage('clear', '*');
-      }
+      },
     },
 
     update: {
-      mol: function (moduleValue) {
+      mol: function(moduleValue) {
         this._currentValue = moduleValue;
         this._currentType = 'mol';
         if (!moduleValue.get()) return;
@@ -152,7 +150,7 @@ define([
           this._initHighlight(moduleValue);
         }
       },
-      jme: function (moduleValue) {
+      jme: function(moduleValue) {
         this._currentValue = moduleValue;
         this._currentType = 'jme';
         if (!moduleValue.get()) return;
@@ -160,65 +158,57 @@ define([
         this.postMessage('setJmeFile', String(moduleValue.get()));
         this._initHighlight(moduleValue);
       },
-      smiles: function (moduleValue) {
+      smiles: function(moduleValue) {
         var that = this;
         this._currentValue = moduleValue;
         this._currentType = 'smiles';
 
-        require(['openchemlib/openchemlib-core'], function (OCL) {
+        require(['openchemlib/openchemlib-core'], function(OCL) {
           var smiles = String(moduleValue.get());
           var mol = OCL.Molecule.fromSmiles(smiles);
           that.postMessage('setMolFile', mol.toMolfile());
           that._initHighlight(moduleValue);
         });
-      }
+      },
     },
 
     onActionReceive: {
-      setOptions: function (val) {
+      setOptions: function(val) {
         this.setJSMEOptions(val);
-      }
+      },
     },
 
-    _initHighlight: function (moduleValue) {
+    _initHighlight: function(moduleValue) {
       var that = this;
       API.killHighlight(this.module.getId());
       API.listenHighlight(
         moduleValue,
-        function (onOff, highlightId) {
+        function(onOff, highlightId) {
           var atoms = [];
           for (var i = 0, l = highlightId.length; i < l; i++) {
-            if (
-              !(
-                moduleValue._atoms[highlightId[i]] instanceof
-                                Array
-              )
-            )
-              moduleValue._atoms[highlightId[i]] = [moduleValue._atoms[highlightId[i]]];
-            atoms = atoms.concat(
-              moduleValue._atoms[highlightId[i]]
-            );
+            if (!(moduleValue._atoms[highlightId[i]] instanceof Array))
+              moduleValue._atoms[highlightId[i]] = [
+                moduleValue._atoms[highlightId[i]],
+              ];
+            atoms = atoms.concat(moduleValue._atoms[highlightId[i]]);
           }
           that.postMessage('setHighlight', {
             atoms: atoms,
-            onOff: onOff
+            onOff: onOff,
           });
         },
         false,
-        this.module.getId()
+        this.module.getId(),
       );
     },
 
-    _doHighlight: function (mol, id) {
+    _doHighlight: function(mol, id) {
       if (!this._currentValue) return;
 
       // there is a problem with overlapping atoms, there is no event out
       // we therefore systematically unhighlight
       for (var i in this._currentValue._atoms) {
-        if (
-          this._currentValue._atoms[i].indexOf(this.highlightedAtom) >
-                    -1
-        ) {
+        if (this._currentValue._atoms[i].indexOf(this.highlightedAtom) > -1) {
           API.highlightId(i, false);
         }
       }
@@ -234,22 +224,22 @@ define([
       this.highlightedAtom = id - 1;
     },
 
-    postMessage: function (type, message) {
+    postMessage: function(type, message) {
       var cw = this.dom.get(0).contentWindow;
       if (cw) {
         cw.postMessage(
           JSON.stringify({
             type: type,
-            message: message
+            message: message,
           }),
-          '*'
+          '*',
         );
       }
     },
 
-    remove: function (id) {
+    remove: function(id) {
       delete views[id];
-    }
+    },
   });
 
   return View;
