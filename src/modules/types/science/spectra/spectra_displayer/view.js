0ca416c6fc21cb82114dddd44a656e5dd9debf6f
Luc Patiny <luc@patiny.com>
2021-09-26 08:50:25 +0200

    chore: fix eslint

--- src/modules/types/science/spectra/spectra_displayer/view.js
+++ src/modules/types/science/spectra/spectra_displayer/view.js
@@ -9,7 +9,7 @@ define([
   'src/util/color',
   'src/util/debug',
   'src/util/util',
-], function($, Default, Graph, JSONChart, API, Color, Debug, Util) {
+], function ($, Default, Graph, JSONChart, API, Color, Debug, Util) {
   const defaultScatterStyle = {
     shape: 'circle',
     cx: 0,
@@ -257,7 +257,7 @@ define([
           xAxis.on('zoom', xZoomHandler);
           xAxis.on('zoomOutFull', xZoomHandler);
           if (cfgCheckbox('FitYToAxisOnFromTo', 'rescale')) {
-            xAxis.on('zoom', function() {
+            xAxis.on('zoom', function () {
               yAxis.scaleToFitAxis(this);
             });
           }
