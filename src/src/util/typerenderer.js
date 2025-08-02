dbd486ba446ca0d5e602897b8444bfa27342f6b3
Michaël Zasso <targos@protonmail.com>
2025-08-02 12:10:39 +0200

    Revert "fix: typerenderer of GHS and force string"
    This reverts commit ff5ec232f97e952dd5fd3fb2d2096c7e62110ce2.

--- src/src/util/typerenderer.js
+++ src/src/util/typerenderer.js
@@ -237,7 +237,6 @@ define([
   functions.elecconfig = {};
   functions.elecconfig.toscreen = function ($element, value) {
     if (value) {
-      value = String(value);
       $element.html(value.replaceAll(/([a-z])([0-9]+)/g, '$1<sup>$2</sup>'));
     } else {
       $element.html('');
@@ -251,12 +250,12 @@ define([
     for (var i = 1; i <= 9; i++) {
       ghs[i] = require.toUrl(`./typerenderer/svg/${i}.svg`);
     }
+
     $element.html('');
 
     if (val) {
+      val = val.replaceAll(/^\s+|\s+$/g, '');
       if (!Array.isArray(val)) {
-        val = String(val);
-        val = val.replaceAll(/^\s+|\s+$/g, '');
         val = val.split(/[\r\n\t,; ]+/);
       }
       for (let ghsValue of val) {
